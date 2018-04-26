d630ad6f4fa27c7a11bf916f4b3070c80ac9054e
Cosmin Basca <cosmin.basca@oracle.com>
2018-04-26 15:21:41 +0200

    add itertools.count implementation

--- graalpython/lib-graalpython/itertools.py
+++ graalpython/lib-graalpython/itertools.py
@@ -48,8 +48,13 @@ class islice(object):
                 return element
 
 
-class count():
-    pass
+def count(start=0, step=1):
+    # count(10) --> 10 11 12 13 14 ...
+    # count(2.5, 0.5) -> 2.5 3.0 3.5 ...
+    n = start
+    while True:
+        yield n
+        n += step
 
 
 class permutations():
