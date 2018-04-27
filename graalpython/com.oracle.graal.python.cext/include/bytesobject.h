e337eaffdae624446b1cea9b86a5a672e3da489d
Florian Angerer <florian.angerer@oracle.com>
2018-04-27 13:49:35 +0200

    Temporarily modify type definition 'PyBytesObject'.

--- graalpython/com.oracle.graal.python.cext/include/bytesobject.h
+++ graalpython/com.oracle.graal.python.cext/include/bytesobject.h
@@ -36,7 +36,7 @@ functions should be applied to nil objects.
 typedef struct {
     PyObject_VAR_HEAD
     Py_hash_t ob_shash;
-    char ob_sval[1];
+    char *ob_sval;
 
     /* Invariants:
      *     ob_sval contains space for 'ob_size+1' elements.
