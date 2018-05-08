6254cd6aedbbde95c6b7faeed733548fef63c1af
Cosmin Basca <cosmin.basca@oracle.com>
2018-05-08 14:54:44 +0200

    fix creation of dict from dict and kwargs, when storage is unmodifiable (of the src dict)

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/HashingStorageNodes.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/HashingStorageNodes.java
@@ -301,9 +301,15 @@ public abstract class HashingStorageNodes {
         }
 
         @Specialization(guards = {"!isNoValue(iterable)", "!isEmpty(kwargs)"})
-        public void doPDictKwargs(PDict self, PDict iterable, PKeyword[] kwargs) {
-            HashingStorage dictStorage = iterable.getDictStorage().copy(HashingStorage.DEFAULT_EQIVALENCE);
-            dictStorage.addAll(new KeywordsStorage(kwargs));
+        public void doPDictKwargs(PDict self, PDict iterable, PKeyword[] kwargs,
+                        @Cached("create()") UnionNode unionNode) {
+            HashingStorage dictStorage;
+            try {
+                dictStorage = iterable.getDictStorage().copy(HashingStorage.DEFAULT_EQIVALENCE);
+                dictStorage.addAll(new KeywordsStorage(kwargs));
+            } catch (HashingStorage.UnmodifiableStorageException e) {
+                dictStorage = unionNode.execute(iterable.getDictStorage(), new KeywordsStorage(kwargs));
+            }
             self.setDictStorage(dictStorage);
         }
 
