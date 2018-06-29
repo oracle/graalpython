ce1a316b68d66ca1613989de8937fe9f62b0007f
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-06-29 10:00:03 +0200

    add version and build info functions

---
+++ graalpython/com.oracle.graal.python.cext/src/getversion.c
@@ -0,0 +1,20 @@
+/* Copyright (c) 2018, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2017 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
+
+/* Return the full version string. */
+
+#include "Python.h"
+
+#include "patchlevel.h"
+
+const char *
+Py_GetVersion(void)
+{
+    static char version[250];
+    PyOS_snprintf(version, sizeof(version), "%.80s (%.80s) %.80s",
+                  PY_VERSION, Py_GetBuildInfo(), Py_GetCompiler());
+    return version;
+}
