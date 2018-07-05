e2776d742f3d23c91ab9ad9b0762c4ed4586e0af
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-07-05 10:32:57 +0200

    add missing methodobject.c

---
+++ graalpython/com.oracle.graal.python.cext/src/methodobject.c
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2018, Oracle and/or its affiliates.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or data
+ * (collectively the "Software"), free of charge and under any and all copyright
+ * rights in the Software, and any and all patent rights owned or freely
+ * licensable by each licensor hereunder covering either (i) the unmodified
+ * Software as contributed to or provided by such licensor, or (ii) the Larger
+ * Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ *     one is included with the Software (each a "Larger Work" to which the
+ *     Software is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#include "capi.h"
+
+typedef PyObject *(*PyCFunction)(PyObject *, PyObject *);
+
+PyTypeObject PyCFunction_Type = PY_TRUFFLE_TYPE("builtin_function_or_method", &PyType_Type, Py_TPFLAGS_DEFAULT | Py_TPFLAGS_HAVE_GC, sizeof(PyCFunctionObject));
+
+PyObject* PyCFunction_NewEx(PyMethodDef *ml, PyObject *self, PyObject *module) {
+    PyObject* func = UPCALL_CEXT_O("PyCFunction_NewEx",
+                                   polyglot_from_string((const char*)(ml->ml_name), SRC_CS),
+                                   ml->ml_meth,
+                                   get_method_flags_cwrapper(ml->ml_flags),
+                                   get_method_flags_wrapper(ml->ml_flags),
+                                   polyglot_from_string((const char*)(ml->ml_doc ? ml->ml_doc : ""), SRC_CS));
+    /* TODO: are the below fields required?
+     * ((PyCFunctionObject*)func)->m_self = self;
+     * ((PyCFunctionObject*)func)->m_module = module;
+     * ((PyCFunctionObject*)func)->m_ml = ml;
+     */
+    return func;
+}
