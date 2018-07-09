b359a8d2819a93d725d633e8affb4faddc389d0c
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-07-09 09:36:06 +0200

    re-add function.__name__

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/FunctionBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/FunctionBuiltins.java
@@ -26,6 +26,7 @@
 
 package com.oracle.graal.python.builtins.objects.function;
 
+import static com.oracle.graal.python.nodes.SpecialAttributeNames.__NAME__;
 import static com.oracle.graal.python.nodes.SpecialMethodNames.__GET__;
 
 import java.util.List;
@@ -36,8 +37,16 @@ import com.oracle.graal.python.builtins.PythonBuiltins;
 import com.oracle.graal.python.builtins.objects.PNone;
 import com.oracle.graal.python.builtins.objects.method.PBuiltinMethod;
 import com.oracle.graal.python.builtins.objects.method.PMethod;
+import com.oracle.graal.python.builtins.objects.str.PString;
+import com.oracle.graal.python.nodes.attributes.ReadAttributeFromObjectNode;
+import com.oracle.graal.python.nodes.attributes.WriteAttributeToObjectNode;
 import com.oracle.graal.python.nodes.function.PythonBuiltinBaseNode;
+import com.oracle.graal.python.nodes.function.PythonBuiltinNode;
 import com.oracle.graal.python.nodes.function.builtins.PythonTernaryBuiltinNode;
+import com.oracle.graal.python.runtime.exception.PythonErrorType;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Fallback;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -84,4 +93,54 @@ public class FunctionBuiltins extends PythonBuiltins {
             return self;
         }
     }
+
+    @Builtin(name = __NAME__, minNumOfArguments = 1, maxNumOfArguments = 2, isGetter = true, isSetter = true)
+    @GenerateNodeFactory
+    abstract static class NameNode extends PythonBuiltinNode {
+        @Child WriteAttributeToObjectNode writeNode;
+
+        @Specialization(guards = "isNoValue(noValue)")
+        Object getName(PFunction self, @SuppressWarnings("unused") PNone noValue,
+                        @Cached("create()") ReadAttributeFromObjectNode readNode) {
+            Object storedName = readNode.execute(self, __NAME__);
+            if (storedName == PNone.NO_VALUE) {
+                return self.getName();
+            } else {
+                return storedName;
+            }
+        }
+
+        @Specialization(guards = "isNoValue(noValue)")
+        Object getName(PBuiltinFunction self, @SuppressWarnings("unused") PNone noValue) {
+            return self.getName();
+        }
+
+        @Specialization
+        Object setName(PFunction self, String value) {
+            if (writeNode == null) {
+                CompilerDirectives.transferToInterpreterAndInvalidate();
+                writeNode = insert(WriteAttributeToObjectNode.create());
+            }
+            writeNode.execute(self, __NAME__, value);
+            return PNone.NONE;
+        }
+
+        @Specialization
+        Object setName(PFunction self, PString value) {
+            return setName(self, value.getValue());
+        }
+
+        @SuppressWarnings("unused")
+        @Specialization
+        Object setName(PBuiltinFunction self, Object value) {
+            throw raise(PythonErrorType.AttributeError, "attribute '__name__' of builtin function is not writable");
+        }
+
+        @SuppressWarnings("unused")
+        @Fallback
+        Object setName(Object self, Object value) {
+            throw raise(PythonErrorType.TypeError, "__name__ must be set to a string object");
+        }
+    }
+
 }
