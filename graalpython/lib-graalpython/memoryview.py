0226ad60d751a032745aaaaf3f2a3fad087bbd40
Florian Angerer <florian.angerer@oracle.com>
2018-07-17 12:30:26 +0200

    Also delegate properties of 'memoryview' to 'nativememoryview'.

--- graalpython/lib-graalpython/memoryview.py
+++ graalpython/lib-graalpython/memoryview.py
@@ -46,30 +46,69 @@ def make_init():
 
     return __memoryview_init
 
+
 def make_getitem():
     def __memoryview_getitem(self, key):
         res = self.__c_memoryview.__getitem__(key)
         return memoryview(res) if isinstance(res, type(self.__c_memoryview)) else res
     return __memoryview_getitem
 
-def make_readonly():
-    class Dummy:
-        @property
-        def readonly(self):
-            return self.__c_memoryview.readonly
 
-        @readonly.setter
-        def readonly(self, value):
-            raise AttributeError("attribute 'readonly' of 'memoryview' objects is not writable")
-    return Dummy.readonly
+def make_property(name):
+    template = """def {0}_getter(self):
+    return self.__c_memoryview.{0}
 
+{0} = property({0}_getter)
+
+def {0}_setter(self, value):
+    raise AttributeError("attribute '{0}' of 'memoryview' objects is not writable")
+
+{0} = {0}.setter({0}_setter)"""
+    # use dict for globals and locals to avoid name conflicts
+    _globals = dict(globals())
+    _locals = dict()
+    exec(template.format(name), _globals, _locals)
+    return eval(name, _globals, _locals)
+
+
+# delegate methods
 memoryview.__init__ = make_init()
 memoryview.__repr__ = lambda self: self.__c_memoryview.__repr__()
 memoryview.__len__ = lambda self: self.__c_memoryview.__len__()
 memoryview.__getitem__ = make_getitem()
 memoryview.__setitem__ = lambda self, key, value: self.__c_memoryview.__setitem__(key, value)
-memoryview.readonly = make_readonly()
+memoryview.release = lambda self: self.__c_memoryview.release()
+memoryview.tobytes = lambda self: self.__c_memoryview.tobytes()
+memoryview.hex = lambda self: self.__c_memoryview.hex()
+memoryview.tolist = lambda self: self.__c_memoryview.tolist()
+memoryview.cast = lambda self, *args: self.__c_memoryview.cast(*args)
+memoryview.__enter__ = lambda self: self.__c_memoryview.__enter__()
+memoryview.__exit__ = lambda self: self.__c_memoryview.__exit__()
+
+# delegate properties
+memoryview.nbytes = make_property("nbytes")
+memoryview.readonly = make_property("readonly")
+memoryview.itemsize = make_property("itemsize")
+memoryview.format = make_property("format")
+memoryview.ndim = make_property("ndim")
+memoryview.shape = make_property("shape")
+memoryview.strides = make_property("strides")
+memoryview.suboffsets = make_property("suboffsets")
+memoryview.c_contiguous = make_property("c_contiguous")
+memoryview.f_contiguous = make_property("f_contiguous")
+memoryview.contiguous = make_property("contiguous")
+
 
 del make_init
 del make_getitem
-del make_readonly
+# del make_nbytes
+# del make_readonly
+# del make_itemsize
+# del make_format
+# del make_ndim
+# del make_shape
+# del make_strides
+# del make_suboffsets
+# del make_c_contiguous
+# del make_f_contiguous
+# del make_contiguous
