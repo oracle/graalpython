756110d1860078cf62353d6a955fc6316f71112e
Cosmin Basca <cosmin.basca@oracle.com>
2018-08-14 11:52:49 +0200

    utility local gate script: update gate command

--- scripts/gate_python_test.sh
+++ scripts/gate_python_test.sh
@@ -40,5 +40,5 @@
 # mx --strict-compliance --dynamicimports sulong --primary gate --tags python-junit,python-unittest
 # 
 # DEFAULT_DYNAMIC_IMPORTS="sulong,/compiler"
-mx --strict-compliance --dynamicimports sulong,/compiler --primary gate --tags python-unittest -B=--force-deprecation-as-warning-for-dependencies
+mx --strict-compliance --dynamicimports sulong,/compiler --primary gate --tags python-junit -B=--force-deprecation-as-warning-for-dependencies
 # mx --dynamicimports /compiler python-gate --tags python-junit,python-unittest
