260766268bb4944713f78146d832af7c3b57175b
Cosmin Basca <cosmin.basca@oracle.com>
2018-10-16 11:45:58 +0200

    add -p/--paths option for harness: set the system path to search for extra modules needed by the benchmark
    fix -p

--- graalpython/benchmarks/src/harness.py
+++ graalpython/benchmarks/src/harness.py
@@ -132,7 +132,6 @@ class BenchRunner(object):
             return attr(*args)
 
     def run(self):
-        print(_HRULE)
         if self._run_once:
             print("### %s, exactly one iteration (no warmup curves)" % (self.bench_module.__name__))
         else:
@@ -163,16 +162,19 @@ class BenchRunner(object):
                 else:
                     print("### iteration=%s, name=%s, duration=%s" % (iteration, self.bench_module.__name__, duration))
 
-        print("teardown ... ")
+        print(_HRULE)
+        print("### teardown ... ")
         self._call_attr(ATTR_TEARDOWN)
-        print("benchmark complete")
+        print("### benchmark complete")
+        print(_HRULE)
 
 
-def run_benchmark(prog, args):
+def run_benchmark(args):
     warmup = 0
     iterations = 1
     bench_file = None
     bench_args = []
+    paths = []
 
     i = 0
     while i < len(args):
@@ -182,19 +184,36 @@ def run_benchmark(prog, args):
             iterations = _as_int(args[i])
         elif arg.startswith("--iterations"):
             iterations = _as_int(arg.split("=")[1])
+
         elif arg == '-w':
             i += 1
             warmup = _as_int(args[i])
         elif arg.startswith("--warmup"):
             warmup = _as_int(arg.split("=")[1])
+
+        elif arg == '-p':
+            i += 1
+            paths = args[i].split(",")
+        elif arg.startswith("--path"):
+            paths = arg.split("=")[1].split(",")
+
         elif bench_file is None:
             bench_file = arg
         else:
             bench_args.append(arg)
         i += 1
 
+    # set the paths if specified
+    print(_HRULE)
+    if paths:
+        for pth in paths:
+            print("### adding module path: %s" % pth)
+            sys.path.append(pth)
+    else:
+        print("### no extra module search paths specified")
+
     BenchRunner(bench_file, bench_args=bench_args, iterations=iterations, warmup=warmup).run()
 
 
 if __name__ == '__main__':
-    run_benchmark(sys.argv[0], sys.argv[1:])
+    run_benchmark(sys.argv[1:])
