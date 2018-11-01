97b361e616a24ba196771a234d87ded8683df48f
Cosmin Basca <cosmin.basca@oracle.com>
2018-11-01 17:32:21 +0100

    fix getting the class __module__ attr - for cases when the attr is not set

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/type/TypeBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/type/TypeBuiltins.java
@@ -111,9 +111,8 @@ public class TypeBuiltins extends PythonBuiltins {
 
         @Specialization
         public String repr(PythonClass self,
-                        @Cached("create(__GETATTRIBUTE__)") LookupAndCallBinaryNode readModuleNode,
                         @Cached("create(__GETATTRIBUTE__)") LookupAndCallBinaryNode readQualNameNode) {
-            Object moduleName = readModuleNode.executeObject(self, __MODULE__);
+            Object moduleName = self.getAttribute(__MODULE__);
             Object qualName = readQualNameNode.executeObject(self, __QUALNAME__);
             return concat(moduleName, qualName);
         }
