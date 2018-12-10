257982611ab4d94514a5f3d7586af05c3c65a56e
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-12-10 11:29:22 +0100

    add two missing functions that are used in more recent numpy

--- graalpython/com.oracle.graal.python.cext/src/object.c
+++ graalpython/com.oracle.graal.python.cext/src/object.c
@@ -388,6 +388,10 @@ PyObject* _PyObject_GC_New(PyTypeObject *tp) {
     return _PyObject_New(tp);
 }
 
+PyVarObject* _PyObject_GC_NewVar(PyTypeObject *tp, Py_ssize_t nitems) {
+    return _PyObject_NewVar(tp, nitems);
+}
+
 PyVarObject *
 _PyObject_NewVar(PyTypeObject *tp, Py_ssize_t nitems)
 {
