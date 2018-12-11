404f81e7fdcbf8fb4bbe4b48f5f6001e3089e1c8
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-12-11 10:22:12 +0100

    stub (m|a)time_ns stat accessors

--- graalpython/lib-graalpython/posix.py
+++ graalpython/lib-graalpython/posix.py
@@ -46,6 +46,9 @@ stat_result = make_named_tuple_class("stat_result", [
     "st_uid", "st_gid", "st_size", "st_atime",
     "st_mtime", "st_ctime"
 ])
+stat_result.st_atime_ns = property(lambda s: int(s.st_atime * 1000))
+stat_result.st_mtime_ns = property(lambda s: int(s.st_mtime * 1000))
+
 old_stat = stat
 
 
