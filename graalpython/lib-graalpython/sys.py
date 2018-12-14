94d1d9915cc949cc707f18c1c1e3de46da9bbf09
Tim Felgentreff <tim.felgentreff@oracle.com>
2018-12-14 11:19:32 +0100

    rename sys.implementation.version_info to sys.implementation.version

--- graalpython/lib-graalpython/sys.py
+++ graalpython/lib-graalpython/sys.py
@@ -45,13 +45,13 @@ def make_implementation_info():
     return SimpleNamespace(
         name="graalpython",
         cache_tag="graalpython",
-        version_info=version_info_type(version_info),
+        version=version_info_type(version_info),
         _multiarch=__gmultiarch
     )
 implementation = make_implementation_info()
 del make_implementation_info
 del __gmultiarch
-version_info = implementation.version_info
+version_info = implementation.version
 
 
 def make_flags_class():
