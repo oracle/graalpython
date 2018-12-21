390ebfecd4cf3350f63bae213a64d48454422958
Petr Pisl <petr.pisl@oracle.com>
2018-12-21 14:09:17 +0100

    [GR-13171] rstrip and lstrip is missing for PBytes.

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/bytes/AbstractBytesBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/bytes/AbstractBytesBuiltins.java
@@ -45,9 +45,12 @@ import com.oracle.graal.python.builtins.Builtin;
 import com.oracle.graal.python.builtins.CoreFunctions;
 import com.oracle.graal.python.builtins.PythonBuiltinClassType;
 import com.oracle.graal.python.builtins.PythonBuiltins;
+import com.oracle.graal.python.builtins.objects.PNone;
 import com.oracle.graal.python.nodes.function.PythonBuiltinBaseNode;
+import com.oracle.graal.python.nodes.function.builtins.PythonBinaryBuiltinNode;
 import com.oracle.graal.python.nodes.function.builtins.PythonUnaryBuiltinNode;
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -114,6 +117,177 @@ public class AbstractBytesBuiltins extends PythonBuiltins {
         @Specialization
         PBytes replace(PBytes self) {
             return factory().createBytes(upper(toBytes.execute(self)));
+        }
+    }
+
+    abstract static class AStripNode extends PythonBinaryBuiltinNode {
+        int mod() {
+            throw new RuntimeException();
+        }
+
+        int stop(@SuppressWarnings("unused") byte[] bs) {
+            throw new RuntimeException();
+        }
+
+        int start(@SuppressWarnings("unused") byte[] bs) {
+            throw new RuntimeException();
+        }
+
+        PByteArray newByteArrayFrom(@SuppressWarnings("unused") byte[] bs, @SuppressWarnings("unused") int i) {
+            throw new RuntimeException();
+        }
+
+        PBytes newBytesFrom(@SuppressWarnings("unused") byte[] bs, @SuppressWarnings("unused") int i) {
+            throw new RuntimeException();
+        }
+
+        private int findIndex(byte[] bs) {
+            int i = start(bs);
+            int stop = stop(bs);
+            for (; i != stop; i += mod()) {
+                if (!isWhitespace(bs[i])) {
+                    break;
+                }
+            }
+            return i;
+        }
+
+        @Specialization
+        PByteArray strip(PByteArray self, @SuppressWarnings("unused") PNone bytes,
+                        @Cached("create()") BytesNodes.ToBytesNode toBytesNode) {
+            byte[] bs = toBytesNode.execute(self);
+            return newByteArrayFrom(bs, findIndex(bs));
+        }
+
+        @Specialization
+        PBytes strip(PBytes self, @SuppressWarnings("unused") PNone bytes,
+                        @Cached("create()") BytesNodes.ToBytesNode toBytesNode) {
+            byte[] bs = toBytesNode.execute(self);
+            return newBytesFrom(bs, findIndex(bs));
+        }
+
+        @CompilerDirectives.TruffleBoundary
+        private static boolean isWhitespace(byte b) {
+            return Character.isWhitespace(b);
+        }
+
+        private int findIndex(byte[] bs, byte[] stripBs) {
+            int i = start(bs);
+            int stop = stop(bs);
+            outer: for (; i != stop; i += mod()) {
+                for (byte b : stripBs) {
+                    if (b == bs[i]) {
+                        continue outer;
+                    }
+                }
+                break;
+            }
+            return i;
+        }
+
+        @Specialization
+        PByteArray strip(PByteArray self, PBytes bytes,
+                        @Cached("create()") BytesNodes.ToBytesNode selfToBytesNode,
+                        @Cached("create()") BytesNodes.ToBytesNode otherToBytesNode) {
+            byte[] stripBs = selfToBytesNode.execute(bytes);
+            byte[] bs = otherToBytesNode.execute(self);
+            return newByteArrayFrom(bs, findIndex(bs, stripBs));
+        }
+
+        @Specialization
+        PBytes strip(PBytes self, PBytes bytes,
+                        @Cached("create()") BytesNodes.ToBytesNode selfToBytesNode,
+                        @Cached("create()") BytesNodes.ToBytesNode otherToBytesNode) {
+            byte[] stripBs = selfToBytesNode.execute(bytes);
+            byte[] bs = otherToBytesNode.execute(self);
+            return newBytesFrom(bs, findIndex(bs, stripBs));
+        }
+
+    }
+
+    @Builtin(name = "lstrip", minNumOfPositionalArgs = 1, maxNumOfPositionalArgs = 2, keywordArguments = {"bytes"})
+    @GenerateNodeFactory
+    abstract static class LStripNode extends AStripNode {
+
+        private byte[] getResultBytes(int i, byte[] bs) {
+            byte[] out;
+            if (i != 0) {
+                int len = bs.length - i;
+                out = new byte[len];
+                System.arraycopy(bs, i, out, 0, len);
+            } else {
+                out = bs;
+            }
+            return out;
+        }
+
+        @Override
+        PByteArray newByteArrayFrom(byte[] bs, int i) {
+            return factory().createByteArray(getResultBytes(i, bs));
+        }
+
+        @Override
+        PBytes newBytesFrom(byte[] bs, int i) {
+            return factory().createBytes(getResultBytes(i, bs));
+        }
+
+        @Override
+        int mod() {
+            return 1;
+        }
+
+        @Override
+        int stop(byte[] bs) {
+            return bs.length;
+        }
+
+        @Override
+        int start(byte[] bs) {
+            return 0;
+        }
+    }
+
+    @Builtin(name = "rstrip", minNumOfPositionalArgs = 1, maxNumOfPositionalArgs = 2, keywordArguments = {"bytes"})
+    @GenerateNodeFactory
+    abstract static class RStripNode extends AStripNode {
+
+        private byte[] getResultBytes(int i, byte[] bs) {
+            byte[] out;
+            int len = i + 1;
+            if (len != bs.length) {
+                out = new byte[len];
+                System.arraycopy(bs, 0, out, 0, len);
+            } else {
+                out = bs;
+            }
+            return out;
+        }
+
+        @Override
+        PByteArray newByteArrayFrom(byte[] bs, int i) {
+            byte[] out = getResultBytes(i, bs);
+            return factory().createByteArray(out);
+        }
+
+        @Override
+        PBytes newBytesFrom(byte[] bs, int i) {
+            byte[] out = getResultBytes(i, bs);
+            return factory().createBytes(out);
+        }
+
+        @Override
+        int mod() {
+            return -1;
+        }
+
+        @Override
+        int stop(byte[] bs) {
+            return -1;
+        }
+
+        @Override
+        int start(byte[] bs) {
+            return bs.length - 1;
         }
     }
 }
