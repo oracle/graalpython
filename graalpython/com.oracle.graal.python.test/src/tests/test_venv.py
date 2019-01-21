758f7c758e6e97c1f87463528947bcb789071da0
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-01-21 17:37:49 +0100

    just run the venv creation, it'll raise if it fails

--- graalpython/com.oracle.graal.python.test/src/tests/test_venv.py
+++ graalpython/com.oracle.graal.python.test/src/tests/test_venv.py
@@ -52,8 +52,7 @@ class VenvTest():
         shutil.rmtree(self.env_dir)
 
     def test_create_and_use_basic_venv(self):
-        create = subprocess.check_output([sys.executable, "-m", "venv", self.env_dir, "--without-pip"])
-        assert create.decode() == "", create
+        subprocess.check_output([sys.executable, "-m", "venv", self.env_dir, "--without-pip"])
         run = subprocess.getoutput(". %s/bin/activate; python -m site" % self.env_dir)
         assert "ENABLE_USER_SITE: False" in run, run
         assert self.env_dir in run, run
