b86eaf92a7c49b2ea2d17fd9ceca445178ba0a3d
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-01-22 10:31:29 +0100

    build a partial graalvm to get an svm image

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -54,15 +54,6 @@ def _get_stdlib_home():
     return os.path.join(SUITE.dir, "graalpython", "lib-python", "3")
 
 
-def _get_svm_binary():
-    return os.path.join(SUITE.dir, "graalpython-svm")
-
-
-def __get_svm_binary_from_graalvm():
-    vmdir = os.path.join(mx.suite("truffle").dir, "..", "vm")
-    return os.path.join(vmdir, "mxbuild", "-".join([mx.get_os(), mx.get_arch()]), "graalpython.image", "graalpython")
-
-
 def _extract_graalpython_internal_options(args):
     non_internal = []
     additional_dists = []
@@ -224,19 +215,15 @@ def find_eclipse():
                 return
 
 
-def python_build_svm(args):
-    mx.run_mx(
-        ["--dynamicimports", "/substratevm,/vm", "build",
-         "--force-deprecation-as-warning", "--dependencies",
-         "GRAAL_MANAGEMENT,graalpython.image"],
-        nonZeroIsFatal=True
-    )
-    shutil.copy(__get_svm_binary_from_graalvm(), _get_svm_binary())
+_SVM_ARGS = ["--dynamicimports", "/vm,/tools,/substratevm",
+             "--disable-polyglot", "--disable-libpolyglot"]
 
 
 def python_svm(args):
-    python_build_svm(args)
-    svm_image = __get_svm_binary_from_graalvm()
+    mx.run_mx(_SVM_ARGS + ["build"])
+    out = mx.OutputCapture()
+    mx.run_mx(_SVM_ARGS + ["graalvm-home"], out=mx.TeeOutputCapture(out))
+    svm_image = os.path.join(out.data.strip(), "bin", "graalpython")
     mx.run([svm_image] + args)
     return svm_image
 
@@ -328,14 +315,8 @@ def graalpython_gate_runner(args, tasks):
 
     with Task('GraalPython Python tests on SVM', tasks, tags=[GraalPythonTags.svmunit]) as task:
         if task:
-            svm_image_name = "./graalpython-svm"
-            if not os.path.exists(svm_image_name):
-                svm_image_name = python_svm(["-h"])
-            llvm_home = mx_subst.path_substitutions.substitute('--native.Dllvm.home=<path:SULONG_LIBS>')
-            args = ["--python.CoreHome=%s" % os.path.join(SUITE.dir, "graalpython", "lib-graalpython"),
-                    "--python.StdLibHome=%s" % os.path.join(SUITE.dir, "graalpython", "lib-python/3"),
-                    llvm_home]
-            run_python_unittests(svm_image_name, args)
+            svm_image_name = python_svm(["-h"])
+            run_python_unittests(svm_image_name)
 
     with Task('GraalPython license header update', tasks, tags=[GraalPythonTags.license]) as task:
         if task:
@@ -360,11 +341,6 @@ def graalpython_gate_runner(args, tasks):
             ])
             if success not in out.data:
                 mx.abort('Output from generated SVM image "' + svm_image + '" did not match success pattern:\n' + success)
-            llvm_home = mx_subst.path_substitutions.substitute('--native.Dllvm.home=<path:SULONG_LIBS>')
-            args = ["--python.CoreHome=%s" % os.path.join(SUITE.dir, "graalpython", "lib-graalpython"),
-                    "--python.StdLibHome=%s" % os.path.join(SUITE.dir, "graalpython", "lib-python/3"),
-                    llvm_home]
-            run_python_unittests(svm_image, args)
 
 
 mx_gate.add_gate_runner(SUITE, graalpython_gate_runner)
@@ -881,7 +857,6 @@ mx.update_commands(SUITE, {
     'python-update-import': [update_import_cmd, 'import name'],
     'delete-graalpython-if-testdownstream': [delete_self_if_testdownstream, ''],
     'python-checkcopyrights': [python_checkcopyrights, 'Make sure code files have copyright notices'],
-    'python-build-svm': [python_build_svm, 'build svm image if it is outdated'],
     'python-svm': [python_svm, 'run python svm image (building it if it is outdated'],
     'punittest': [punittest, ''],
     'python3-unittests': [python3_unittests, 'run the cPython stdlib unittests'],
