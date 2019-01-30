ad9d3d373930d203fc2cf227908ea50a76043cf9
Florian Angerer <florian.angerer@oracle.com>
2019-01-30 10:11:47 +0100

    Fix copyrights.

--- graalpython/com.oracle.graal.python.test/src/tests/test_mmap.py
+++ graalpython/com.oracle.graal.python.test/src/tests/test_mmap.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2019, Oracle and/or its affiliates.
+# Copyright (C) 1996-2017 Python Software Foundation
+#
+# Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
 from test.support import (TESTFN, run_unittest, import_module, unlink,
                           requires, _2G, _4G, gc_collect, cpython_only)
 import unittest
