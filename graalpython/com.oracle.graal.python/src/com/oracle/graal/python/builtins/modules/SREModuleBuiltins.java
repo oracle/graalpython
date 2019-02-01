532facd4ce1773de8cc884866bc59ed482588bb8
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-02-01 09:48:50 +0100

    use interoplibrary in _sre module

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/SREModuleBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/SREModuleBuiltins.java
@@ -73,12 +73,11 @@ import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.dsl.TypeSystemReference;
 import com.oracle.truffle.api.interop.ArityException;
-import com.oracle.truffle.api.interop.ForeignAccess;
-import com.oracle.truffle.api.interop.Message;
+import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
 import com.oracle.truffle.api.interop.UnsupportedMessageException;
 import com.oracle.truffle.api.interop.UnsupportedTypeException;
-import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.profiles.BranchProfile;
 import com.oracle.truffle.api.source.Source;
 import com.oracle.truffle.regex.RegexSyntaxException;
@@ -197,9 +196,9 @@ public class SREModuleBuiltins extends PythonBuiltins {
         Object call(TruffleObject callable, Object arg1, Object arg2,
                         @Cached("create()") BranchProfile syntaxError,
                         @Cached("create()") BranchProfile typeError,
-                        @Cached("createExecute()") Node invokeNode) {
+                        @CachedLibrary(limit = "1") InteropLibrary interop) {
             try {
-                return ForeignAccess.sendExecute(invokeNode, callable, new Object[]{arg1, arg2});
+                return interop.execute(callable, arg1, arg2);
             } catch (ArityException | UnsupportedTypeException | UnsupportedMessageException e) {
                 typeError.enter();
                 throw raise(TypeError, "%s", e);
@@ -218,10 +217,6 @@ public class SREModuleBuiltins extends PythonBuiltins {
         Object call(Object callable, Object arg1, Object arg2) {
             throw raise(RuntimeError, "invalid arguments passed to tregex_call_compile");
         }
-
-        protected static Node createExecute() {
-            return Message.EXECUTE.createNode();
-        }
     }
 
     @Builtin(name = "tregex_call_exec", fixedNumOfPositionalArgs = 3)
@@ -232,9 +227,9 @@ public class SREModuleBuiltins extends PythonBuiltins {
         @Specialization(guards = "isForeignObject(callable)")
         Object call(TruffleObject callable, Object arg1, Number arg2,
                         @Cached("create()") BranchProfile typeError,
-                        @Cached("createExecute()") Node invokeNode) {
+                        @CachedLibrary(limit = "1") InteropLibrary interop) {
             try {
-                return ForeignAccess.sendExecute(invokeNode, callable, new Object[]{arg1, arg2});
+                return interop.execute(callable, arg1, arg2);
             } catch (ArityException | UnsupportedTypeException | UnsupportedMessageException e) {
                 typeError.enter();
                 throw raise(TypeError, "%s", e);
@@ -245,10 +240,6 @@ public class SREModuleBuiltins extends PythonBuiltins {
         @Fallback
         Object call(Object callable, Object arg1, Object arg2) {
             throw raise(RuntimeError, "invalid arguments passed to tregex_call_exec");
-        }
-
-        protected static Node createExecute() {
-            return Message.EXECUTE.createNode();
         }
     }
 }
