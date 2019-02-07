8b29d83f967d6eb12c29b50dfc71577f1018fde7
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-02-07 10:39:49 +0100

    minor cleanup

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/type/TypeBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/type/TypeBuiltins.java
@@ -219,10 +219,10 @@ public class TypeBuiltins extends PythonBuiltins {
         @Specialization(replaces = "doItUnboxed")
         protected Object doItUnboxedIndirect(VirtualFrame frame, @SuppressWarnings("unused") PNone noSelf, Object[] arguments, PKeyword[] keywords) {
             Object self = arguments[0];
-            if (self instanceof PythonAbstractClass) {
+            if (PGuards.isClass(self)) {
                 return op(frame, (PythonAbstractClass) self, arguments, keywords, false);
             } else if (self instanceof PythonBuiltinClassType) {
-                PythonBuiltinClass actual = getContext().getCore().lookupType((PythonBuiltinClassType) self);
+                PythonBuiltinClass actual = getBuiltinPythonClass((PythonBuiltinClassType) self);
                 return op(frame, actual, arguments, keywords, false);
             } else {
                 throw raise(TypeError, "descriptor '__call__' requires a 'type' object but received a '%p'", self);
