672e5919792b4795c011c7c224022ecac78a6a36
Cosmin Basca <cosmin.basca@oracle.com>
2019-02-15 19:37:42 +0100

    truffle library migration: PyMethodDescrWrapper read / write message

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/PyMethodDescrWrapper.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/PyMethodDescrWrapper.java
@@ -40,15 +40,35 @@
  */
 package com.oracle.graal.python.builtins.objects.cext;
 
+import com.oracle.graal.python.builtins.objects.PNone;
 import com.oracle.graal.python.builtins.objects.cext.NativeWrappers.PythonNativeWrapper;
+import com.oracle.graal.python.builtins.objects.method.PBuiltinMethod;
 import com.oracle.graal.python.builtins.objects.object.PythonObject;
-import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.graal.python.nodes.PGuards;
+import com.oracle.graal.python.nodes.SpecialAttributeNames;
+import com.oracle.graal.python.nodes.SpecialMethodNames;
+import com.oracle.graal.python.nodes.attributes.ReadAttributeFromObjectNode;
+import com.oracle.graal.python.nodes.call.special.LookupAndCallBinaryNode;
+import com.oracle.graal.python.nodes.call.special.LookupAndCallTernaryNode;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.ImportStatic;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.interop.UnknownIdentifierException;
+import com.oracle.truffle.api.interop.UnsupportedMessageException;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.nodes.Node;
 
 /**
  * Wraps a PythonObject to provide a native view with a shape like {@code PyMethodDescr}.
  */
+@ExportLibrary(InteropLibrary.class)
 public class PyMethodDescrWrapper extends PythonNativeWrapper {
+    public static final String NAME = "ml_name";
+    public static final String DOC = "ml_doc";
 
     public PyMethodDescrWrapper(PythonObject delegate) {
         super(delegate);
@@ -58,8 +78,123 @@ public class PyMethodDescrWrapper extends PythonNativeWrapper {
         return o instanceof PyMethodDescrWrapper;
     }
 
-    @Override
-    public ForeignAccess getForeignAccess() {
-        return PyMethodDescrWrapperMRForeign.ACCESS;
+    @ExportMessage
+    boolean hasMembers() {
+        return true;
+    }
+
+    @ExportMessage
+    protected boolean isMemberReadable(String member) {
+        switch (member) {
+            case NAME:
+            case DOC:
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    @ExportMessage
+    protected Object getMembers(boolean includeInternal) throws UnsupportedMessageException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ExportMessage
+    protected Object readMember(String member,
+                                @Cached.Exclusive @Cached(allowUncached = true) ReadFieldNode readFieldNode) {
+        return readFieldNode.execute(this.getDelegate(), member);
+    }
+
+    @ImportStatic({SpecialMethodNames.class})
+    abstract static class ReadFieldNode extends Node {
+        public static final String NAME = PyMethodDescrWrapper.NAME;
+        public static final String DOC = PyMethodDescrWrapper.DOC;
+
+        public abstract Object execute(Object delegate, String key);
+
+        protected boolean eq(String expected, String actual) {
+            return expected.equals(actual);
+        }
+
+        @Specialization(guards = {"eq(NAME, key)"})
+        Object getName(PythonObject object, @SuppressWarnings("unused") String key,
+                       @Cached("key") @SuppressWarnings("unused") String cachedKey,
+                       @Cached("create(__GETATTRIBUTE__)") LookupAndCallBinaryNode getAttrNode,
+                       @Cached.Shared("toSulongNode") @Cached CExtNodes.ToSulongNode toSulongNode,
+                       @Cached.Shared("asCharPointerNode") @Cached CExtNodes.AsCharPointer asCharPointerNode) {
+            Object doc = getAttrNode.executeObject(object, SpecialAttributeNames.__NAME__);
+            if (doc == PNone.NONE) {
+                return toSulongNode.execute(PNone.NO_VALUE);
+            } else {
+                return asCharPointerNode.execute(doc);
+            }
+        }
+
+        @Specialization(guards = {"eq(DOC, key)"})
+        Object getDoc(PythonObject object, @SuppressWarnings("unused") String key,
+                      @Cached("key") @SuppressWarnings("unused") String cachedKey,
+                      @Cached("create()") ReadAttributeFromObjectNode getAttrNode,
+                      @Cached.Shared("toSulongNode") @Cached CExtNodes.ToSulongNode toSulongNode,
+                      @Cached.Shared("asCharPointerNode") @Cached CExtNodes.AsCharPointer asCharPointerNode) {
+            Object doc = getAttrNode.execute(object, SpecialAttributeNames.__DOC__);
+            if (doc instanceof PNone) {
+                return toSulongNode.execute(PNone.NO_VALUE);
+            } else {
+                return asCharPointerNode.execute(doc);
+            }
+        }
+    }
+
+    @ExportMessage
+    protected boolean isMemberModifiable(String member) {
+        return member.equals(DOC);
+    }
+
+    @ExportMessage
+    protected boolean isMemberInsertable(String member) {
+        return member.equals(DOC);
+    }
+
+    @ExportMessage
+    protected void writeMember(String member, Object value,
+                               @Cached.Exclusive @Cached(allowUncached = true) WriteFieldNode writeFieldNode) {
+        writeFieldNode.execute(this.getDelegate(), member, value);
+    }
+
+    @ExportMessage
+    protected boolean isMemberRemovable(String member) {
+        return false;
+    }
+
+    @ExportMessage
+    protected void removeMember(String member) throws UnsupportedMessageException, UnknownIdentifierException {
+        throw UnsupportedMessageException.create();
+    }
+
+    @ImportStatic({SpecialMethodNames.class, PGuards.class})
+    abstract static class WriteFieldNode extends Node {
+        public static final String DOC = PyMethodDescrWrapper.DOC;
+
+        public abstract void execute(Object delegate, String key, Object value);
+
+        protected boolean eq(String expected, String actual) {
+            return expected.equals(actual);
+        }
+
+        @Specialization(guards = {"!isBuiltinMethod(object)", "eq(DOC, key)"})
+        void getDoc(PythonObject object, @SuppressWarnings("unused") String key, Object value,
+                    @Cached("key") @SuppressWarnings("unused") String cachedKey,
+                    @Cached("create(__SETATTR__)") LookupAndCallTernaryNode setAttrNode,
+                    @Cached.Shared("fromCharPointerNode") @Cached CExtNodes.FromCharPointerNode fromCharPointerNode) {
+            setAttrNode.execute(object, SpecialAttributeNames.__DOC__, fromCharPointerNode.execute(value));
+        }
+
+        @Specialization(guards = {"eq(DOC, key)"})
+        void getDoc(PBuiltinMethod object, @SuppressWarnings("unused") String key, Object value,
+                    @Cached("key") @SuppressWarnings("unused") String cachedKey,
+                    @Cached.Shared("fromCharPointerNode") @Cached CExtNodes.FromCharPointerNode fromCharPointerNode) {
+            CompilerDirectives.transferToInterpreter();
+            object.getStorage().define(SpecialAttributeNames.__DOC__, fromCharPointerNode.execute(value));
+        }
     }
 }
