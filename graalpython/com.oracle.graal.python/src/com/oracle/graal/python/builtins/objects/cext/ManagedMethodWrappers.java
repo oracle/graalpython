7618e7101d4c817bd44860b6af3f934a56861d3f
Cosmin Basca <cosmin.basca@oracle.com>
2019-02-15 19:20:25 +0100

    truffle library migration: ManagedMethodWrappers execute / ispointer / aspointer / isnative

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/ManagedMethodWrappers.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/ManagedMethodWrappers.java
@@ -41,14 +41,30 @@
 package com.oracle.graal.python.builtins.objects.cext;
 
 import com.oracle.graal.python.builtins.objects.cext.NativeWrappers.PythonNativeWrapper;
-import com.oracle.truffle.api.interop.ForeignAccess;
+import com.oracle.graal.python.builtins.objects.function.PKeyword;
+import com.oracle.graal.python.nodes.argument.keywords.ExecuteKeywordStarargsNode;
+import com.oracle.graal.python.nodes.argument.positional.ExecutePositionalStarargsNode;
+import com.oracle.graal.python.nodes.argument.positional.PositionalArgumentsNode;
+import com.oracle.graal.python.nodes.call.CallNode;
+import com.oracle.graal.python.runtime.interop.PythonMessageResolution;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.Specialization;
+import com.oracle.truffle.api.interop.ArityException;
+import com.oracle.truffle.api.interop.InteropLibrary;
 import com.oracle.truffle.api.interop.TruffleObject;
+import com.oracle.truffle.api.library.ExportLibrary;
+import com.oracle.truffle.api.library.ExportMessage;
+import com.oracle.truffle.api.nodes.ExplodeLoop;
+import com.oracle.truffle.api.nodes.Node;
+import com.oracle.truffle.api.profiles.PrimitiveValueProfile;
 
 /**
  * Wrappers for methods used by native code.
  */
 public abstract class ManagedMethodWrappers {
 
+    @ExportLibrary(InteropLibrary.class)
     public abstract static class MethodWrapper extends PythonNativeWrapper {
 
         public MethodWrapper(Object method) {
@@ -59,23 +75,120 @@ public abstract class ManagedMethodWrappers {
             return o instanceof MethodWrapper;
         }
 
-        @Override
-        public ForeignAccess getForeignAccess() {
-            return ManagedMethodWrappersMRForeign.ACCESS;
+        @ExportMessage
+        public boolean isPointer(@Cached.Exclusive @Cached(allowUncached = true) CExtNodes.IsPointerNode pIsPointerNode) {
+            return pIsPointerNode.execute(this);
+        }
+
+        @ExportMessage
+        public long asPointer(@Cached.Exclusive @Cached(allowUncached = true) PythonObjectNativeWrapperMR.PAsPointerNode pAsPointerNode) {
+            return pAsPointerNode.execute(this);
+        }
+
+        @ExportMessage
+        public void toNative(@Cached.Exclusive @Cached(allowUncached = true) PythonObjectNativeWrapperMR.ToPyObjectNode toPyObjectNode) {
+            toPyObjectNode.execute(this);
         }
     }
 
+    @ExportLibrary(InteropLibrary.class)
     static class MethKeywords extends MethodWrapper {
 
         public MethKeywords(Object method) {
             super(method);
         }
+
+        @ExportMessage
+        public boolean isExecutable() {
+            return true;
+        }
+
+        @ExportMessage
+        public Object execute(Object[] arguments,
+                              @Cached.Exclusive @Cached(allowUncached = true) ExecuteNode executeNode) {
+            return executeNode.execute(this, arguments);
+        }
+
+        abstract static class ExecuteNode extends Node {
+            public abstract Object execute(MethKeywords object, Object[] arguments);
+
+            @Specialization
+            public Object execute(MethKeywords object, Object[] arguments,
+                                  @Cached.Exclusive @Cached CExtNodes.ToJavaNode toJavaNode,
+                                  @Cached.Exclusive @Cached CExtNodes.ToSulongNode toSulongNode,
+                                  @Cached.Exclusive @Cached CallNode dispatch,
+                                  @Cached.Exclusive @Cached ExecutePositionalStarargsNode posStarargsNode,
+                                  @Cached.Exclusive @Cached ExecuteKeywordStarargsNode expandKwargsNode,
+                                  @Cached.Exclusive @Cached("createEqualityProfile()") PrimitiveValueProfile starArgsLenProfile) {
+                if (arguments.length != 3) {
+                    throw ArityException.raise(3, arguments.length);
+                }
+
+                // convert args
+
+                Object receiver = toJavaNode.execute(arguments[0]);
+                Object starArgs = toJavaNode.execute(arguments[1]);
+                Object kwArgs = toJavaNode.execute(arguments[2]);
+
+                Object[] starArgsArray = posStarargsNode.executeWith(starArgs);
+                int starArgsLen = starArgsLenProfile.profile(starArgsArray.length);
+                Object[] pArgs = PositionalArgumentsNode.prependArgument(receiver, starArgsArray, starArgsLen);
+                PKeyword[] kwArgsArray = expandKwargsNode.executeWith(kwArgs);
+
+                // execute
+
+                if (dispatch == null) {
+                    CompilerDirectives.transferToInterpreterAndInvalidate();
+                    dispatch = insert(CallNode.create());
+                }
+                return toSulongNode.execute(dispatch.execute(null, object.getDelegate(), pArgs, kwArgsArray));
+            }
+        }
     }
 
+    @ExportLibrary(InteropLibrary.class)
     static class MethVarargs extends MethodWrapper {
 
         public MethVarargs(Object method) {
             super(method);
+        }
+
+        @ExportMessage
+        public boolean isExecutable() {
+            return true;
+        }
+
+        @ExportMessage
+        public Object execute(Object[] arguments,
+                              @Cached.Exclusive @Cached(allowUncached = true) ExecuteNode executeNode) {
+            return executeNode.execute(this, arguments);
+        }
+
+        abstract static class ExecuteNode extends Node {
+            public abstract Object execute(MethVarargs object, Object[] arguments);
+
+            @Specialization
+            @ExplodeLoop
+            public Object execute(MethVarargs object, Object[] arguments,
+                                  @Cached.Exclusive @Cached CExtNodes.ToJavaNode toJavaNode,
+                                  @Cached.Exclusive @Cached CExtNodes.ToSulongNode toSulongNode,
+                                  @Cached.Exclusive @Cached PythonMessageResolution.ExecuteNode executeNode) {
+                if (arguments.length != 1) {
+                    throw ArityException.raise(1, arguments.length);
+                }
+
+                // convert args
+
+                Object varArgs = toJavaNode.execute(arguments[0]);
+
+                // execute
+
+                if (executeNode == null) {
+                    CompilerDirectives.transferToInterpreterAndInvalidate();
+                    executeNode = insert(new PythonMessageResolution.ExecuteNode());
+                }
+                return toSulongNode.execute(executeNode.execute(object.getDelegate(), new Object[]{varArgs}));
+            }
         }
     }
 
@@ -92,5 +205,6 @@ public abstract class ManagedMethodWrappers {
     public static MethodWrapper createVarargs(Object method) {
         return new MethVarargs(method);
     }
+
 
 }
