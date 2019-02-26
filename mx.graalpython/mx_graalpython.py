4390139ace90f7b1089220e52d327b598fa0b2c7
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-02-26 15:46:18 +0100

    just don't clean up after ourselves in the test

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -434,9 +434,6 @@ def run_shared_lib_test(args=None):
                 return status;
             }
 
-            poly_delete_reference(isolate_thread, engine_builder);
-            poly_delete_reference(isolate_thread, builder);
-
             return poly_ok;
         }
 
@@ -446,17 +443,7 @@ def run_shared_lib_test(args=None):
                 return status;
             }
 
-            status = poly_delete_reference(isolate_thread, context);
-            if (status != poly_ok) {
-                return status;
-            }
-
             status = poly_engine_close(isolate_thread, engine, true);
-            if (status != poly_ok) {
-                return status;
-            }
-
-            status = poly_delete_reference(isolate_thread, engine);
             if (status != poly_ok) {
                 return status;
             }
@@ -483,10 +470,7 @@ def run_shared_lib_test(args=None):
             int32_t result_value;
             poly_value_as_int32(isolate_thread, value, &result_value);
 
-            assert_ok("primitive free failed", poly_delete_reference(isolate_thread, primitive_object) == poly_ok);
-            assert_ok("value free failed", poly_delete_reference(isolate_thread, value) == poly_ok);
             assert_ok("value computation was incorrect", result_value == 42 * 42);
-            assert_ok("func free failed", poly_delete_reference(isolate_thread, func) == poly_ok);
             assert_ok("Context tear down failed.", tear_down_context() == poly_ok);
             return 0;
         }
