53b843cb94e44b1572219ad3516de080abcaa3b5
Florian Angerer <florian.angerer@oracle.com>
2019-03-11 15:44:05 +0100

    truffle library migration: SequenceNodes.LenNode

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/SequenceNodes.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/SequenceNodes.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -41,15 +41,14 @@
 package com.oracle.graal.python.builtins.objects.common;
 
 import com.oracle.graal.python.builtins.objects.common.SequenceNodesFactory.GetSequenceStorageNodeGen;
-import com.oracle.graal.python.builtins.objects.common.SequenceNodesFactory.LenNodeGen;
 import com.oracle.graal.python.builtins.objects.range.PRange;
 import com.oracle.graal.python.builtins.objects.str.PString;
 import com.oracle.graal.python.nodes.PGuards;
-import com.oracle.graal.python.nodes.PNodeWithContext;
 import com.oracle.graal.python.runtime.sequence.PSequence;
 import com.oracle.graal.python.runtime.sequence.storage.SequenceStorage;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Cached;
+import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.ImportStatic;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.nodes.Node;
@@ -57,8 +56,9 @@ import com.oracle.truffle.api.profiles.ValueProfile;
 
 public abstract class SequenceNodes {
 
+    @GenerateUncached
     @ImportStatic(PGuards.class)
-    public abstract static class LenNode extends PNodeWithContext {
+    public abstract static class LenNode extends Node {
 
         public abstract int execute(PSequence seq);
 
@@ -75,12 +75,16 @@ public abstract class SequenceNodes {
 
         @Specialization(guards = {"!isPString(seq)", "!isPRange(seq)"})
         int doWithStorage(PSequence seq,
-                        @Cached("create()") SequenceStorageNodes.LenNode lenNode) {
+                        @Cached SequenceStorageNodes.LenNode lenNode) {
             return lenNode.execute(seq.getSequenceStorage());
         }
 
         public static LenNode create() {
-            return LenNodeGen.create();
+            return SequenceNodesFactory.LenNodeGen.create();
+        }
+
+        public static LenNode getUncached() {
+            return SequenceNodesFactory.LenNodeGen.getUncached();
         }
     }
 
