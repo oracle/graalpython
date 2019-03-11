3861a51d5b8a56a34f2e3aab95f04b276d1e7662
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-03-11 11:12:32 +0100

    start next dev cycle

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "1.0.0-rc14",
+    "version": "1.0.0-rc15",
     "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
