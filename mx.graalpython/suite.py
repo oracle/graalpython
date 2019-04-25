5447b96b47c8262cb55c96bc3470c7038d74991a
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-04-25 09:25:09 +0200

    [GR-15461] Set versions for next dev cycle.

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "1.0.0-rc17",
+    "version": "1.0.0-rc18",
     "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
