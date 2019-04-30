1be69c9cc48fbb69c3089d88ae3bfa7eef6f3af5
Kolya Opahle <kolyaopahle@gmail.com>
2019-04-30 12:59:07 +0200

    fixed isUpper to behave more like the python equivalent

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/str/StringBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/str/StringBuiltins.java
@@ -1818,21 +1818,21 @@ public final class StringBuiltins extends PythonBuiltins {
         @Specialization
         @TruffleBoundary
         boolean doString(String self) {
-            int spaces = 0;
+            int uncased = 0;
             if (self.length() == 0) {
                 return false;
             }
             for (int i = 0; i < self.length(); i++) {
                 int codePoint = self.codePointAt(i);
                 if (!Character.isUpperCase(codePoint)) {
-                    if (Character.isWhitespace(codePoint)) {
-                        spaces++;
+                    if (Character.toLowerCase(codePoint) == Character.toUpperCase(codePoint)) {
+                        uncased++;
                     } else {
                         return false;
                     }
                 }
             }
-            return spaces == 0 || self.length() > spaces;
+            return uncased == 0 || self.length() > uncased;
 
         }
     }
