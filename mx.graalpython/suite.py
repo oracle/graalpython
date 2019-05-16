afa9e1f4ca9032d0125e0fd2eb75e5980ee2ba13
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-05-16 18:00:06 +0200

    Set versions for next dev cycle

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "1.0.0-rc18",
+    "version": "20.0.0-beta.1",
     "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
