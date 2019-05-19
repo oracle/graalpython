712fb652b659bf95123b6fa1d8f172201b46dbdd
Danilo Ansaloni <danilo.ansaloni@oracle.com>
2019-05-19 13:56:11 +0200

    Update version number.

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "20.0.0-beta.1",
+    "version": "20.0.0-beta.01",
     "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
