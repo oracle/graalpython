5e3ff3cfeddd64637af77ccc79d8d4916bfedb97
Franziska Geiger <franziska.geiger@oracle.com>
2019-05-22 09:32:02 +0200

    Copyright fixes

--- graalpython/com.oracle.graal.python.cext/include/weakrefobject.h
+++ graalpython/com.oracle.graal.python.cext/include/weakrefobject.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2019, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2017 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
 /* Weak references objects for Python. */
 
 #ifndef Py_WEAKREFOBJECT_H
