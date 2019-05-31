a098492ecf0d5f48101b43c90d6788ebdb60701b
Cosmin Basca <cosmin.basca@oracle.com>
2019-05-31 13:38:22 +0200

    ci: add more utils functions

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -908,8 +908,10 @@ def import_python_sources(args):
 # add ci verification util
 #
 # ----------------------------------------------------------------------------------------------------------------------
-def verify_ci(dest_suite, args=None):
+def verify_ci(dest_suite, common_dirs=None, args=None):
     """Verify CI configuration"""
+    if common_dirs is None:
+        common_dirs = ['ci_common']
     base_suite = SUITE
     if not isinstance(dest_suite, mx.SourceSuite) or not isinstance(base_suite, mx.SourceSuite):
         raise mx.abort("Can not use verify-ci on binary suites: {} and {} need to be source suites".format(
@@ -917,7 +919,7 @@ def verify_ci(dest_suite, args=None):
     assert isinstance(base_suite, mx.SourceSuite)
     for ext in [".libsonnet", ".jsonnet"]:
         mx.log("CI setup verifying *{} files ... ".format(ext))
-        mx.verify_ci(args, base_suite, dest_suite, common_dirs=['ci_common'], extension=ext)
+        mx.verify_ci(args, base_suite, dest_suite, common_dirs=common_dirs, extension=ext)
 
 
 # ----------------------------------------------------------------------------------------------------------------------
