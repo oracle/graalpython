27b0a3132f938916c9cc1498aeaa1928742f2df4
Florian Angerer <florian.angerer@oracle.com>
2019-08-08 15:35:37 +0200

    Limit inlining of builtins to avoid compilation bailouts for large functions.

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
@@ -160,10 +160,12 @@ public final class PythonLanguage extends TruffleLanguage<PythonContext> {
     protected boolean areOptionsCompatible(OptionValues firstOptions, OptionValues newOptions) {
         // internal sources were marked during context initialization
         return (firstOptions.get(PythonOptions.ExposeInternalSources).equals(newOptions.get(PythonOptions.ExposeInternalSources)) &&
-                        // we cache WithThread on the lanugage
+                        // we cache WithThread on the language
                         firstOptions.get(PythonOptions.WithThread).equals(newOptions.get(PythonOptions.WithThread)) &&
                         // we cache CatchAllExceptions hard on TryExceptNode
-                        firstOptions.get(PythonOptions.CatchAllExceptions).equals(newOptions.get(PythonOptions.CatchAllExceptions)));
+                        firstOptions.get(PythonOptions.CatchAllExceptions).equals(newOptions.get(PythonOptions.CatchAllExceptions)) &&
+                        // we cache BuiltinsInliningMaxCallerSize on the language
+                        firstOptions.get(PythonOptions.BuiltinsInliningMaxCallerSize).equals(newOptions.get(PythonOptions.BuiltinsInliningMaxCallerSize)));
     }
 
     private boolean areOptionsCompatibleWithPreinitializedContext(OptionValues firstOptions, OptionValues newOptions) {
