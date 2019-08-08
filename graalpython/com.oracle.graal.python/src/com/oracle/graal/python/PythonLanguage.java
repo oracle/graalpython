655f139357f6c01659462ad0d2feacfe31be1dc0
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-08-08 09:15:16 +0200

    update micro number

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
@@ -107,7 +107,7 @@ public final class PythonLanguage extends TruffleLanguage<PythonContext> {
     public static final String NAME = "Python";
     public static final int MAJOR = 3;
     public static final int MINOR = 7;
-    public static final int MICRO = 3;
+    public static final int MICRO = 4;
     public static final String VERSION = MAJOR + "." + MINOR + "." + MICRO;
 
     public static final String MIME_TYPE = "text/x-python";
