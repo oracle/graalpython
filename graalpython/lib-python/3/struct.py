34685a108975a0554ef93409c82474cb08116584
Florian Angerer <florian.angerer@oracle.com>
2019-08-14 18:02:30 +0200

    Revert "Remove lazy _struct symbols and explicitly report ImportError."
    This reverts commit f3da0e55bc3c41655e10975fd008959239d5b6c6.

--- graalpython/lib-python/3/struct.py
+++ graalpython/lib-python/3/struct.py
@@ -11,23 +11,20 @@ __all__ = [
     ]
 
 
-# __cstruct = None
-#
-# def make_delegate(p):
-#     def delegate(*args ,**kwargs):
-#         global __cstruct
-#         if not __cstruct:
-#             import _struct as __cstruct
-#         return getattr(__cstruct, p)(*args, **kwargs)
-#     delegate.__name__ = p
-#     return delegate
-#
-# for p in __all__:
-#     globals()[p] = make_delegate(p)
+__cstruct = None
 
-try:
-    from _struct import *
-    from _struct import _clearcache
-    from _struct import __doc__
-except BaseException:
-    raise ImportError
+def make_delegate(p):
+    def delegate(*args ,**kwargs):
+        global __cstruct
+        if not __cstruct:
+            import _struct as __cstruct
+        return getattr(__cstruct, p)(*args, **kwargs)
+    delegate.__name__ = p
+    return delegate
+
+for p in __all__:
+    globals()[p] = make_delegate(p)
+
+#from _struct import *
+#from _struct import _clearcache
+#from _struct import __doc__
