f8a76a3b51c141c9199f9843dcaa5c52d8b22ab1
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2019-08-19 12:04:08 +0200

    Bump version

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,8 +9,8 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "19.2.0",
-    "release": True,
+    "version": "19.2.1",
+    "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
 
