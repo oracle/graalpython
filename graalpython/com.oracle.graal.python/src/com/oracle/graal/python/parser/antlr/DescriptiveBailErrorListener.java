e29fee93b9639490df60bcd304d546d491c40b30
Petr Pisl <petr.pisl@oracle.com>
2019-09-25 14:37:48 +0200

    If we don't know the reason, just raise invalid syntax to pass more tests.

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/parser/antlr/DescriptiveBailErrorListener.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/parser/antlr/DescriptiveBailErrorListener.java
@@ -63,7 +63,7 @@ public class DescriptiveBailErrorListener extends BaseErrorListener {
 
         String entireMessage = String.format("source: %s, line: %s, index: %s, error message: %s %s",
                         recognizer.getInputStream().getSourceName(), line, charPositionInLine, msg,
-                        e == null ? "<null>" : e.getMessage());
+                        e == null || e.getMessage() == null ? "invalid syntax" : e.getMessage());
 
         if (e != null) {
             PIncompleteSourceException handleRecognitionException = handleRecognitionException(e.getExpectedTokens(), entireMessage, e, line);
