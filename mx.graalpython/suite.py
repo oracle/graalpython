402d7640f09dc5c6c006f9f50bb44635f6440c2b
Mohamed EZ-ZARGHILI <mohamed.ez.zarghili@oracle.com>
2019-12-19 12:02:38 +0100

    Start a new dev cycle.

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,8 +9,8 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "19.3.0.1",
-    "release": True,
+    "version": "19.3.0.2",
+    "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
 
