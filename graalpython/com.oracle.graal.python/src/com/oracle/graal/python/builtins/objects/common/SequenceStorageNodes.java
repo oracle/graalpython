65240fd2921632b572cf396c0ed2bf3243803881
Tim Felgentreff <tim.felgentreff@oracle.com>
2019-12-20 14:42:48 +0100

    sequence accesses should raise IndexError on overflow

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/SequenceStorageNodes.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/SequenceStorageNodes.java
@@ -477,7 +477,7 @@ public abstract class SequenceStorageNodes {
         }
 
         protected final int normalizeIndex(VirtualFrame frame, Object idx, SequenceStorage store) {
-            int intIdx = getLibrary().asSizeWithState(idx, PArguments.getThreadState(frame));
+            int intIdx = getLibrary().asSizeWithState(idx, PythonBuiltinClassType.IndexError, PArguments.getThreadState(frame));
             if (normalizeIndexNode != null) {
                 return normalizeIndexNode.execute(intIdx, getStoreProfile().profile(store).length());
             }
@@ -492,7 +492,7 @@ public abstract class SequenceStorageNodes {
         }
 
         protected final int normalizeIndex(@SuppressWarnings("unused") VirtualFrame frame, long idx, SequenceStorage store) {
-            int intIdx = getLibrary().asSize(idx);
+            int intIdx = getLibrary().asSize(idx, PythonBuiltinClassType.IndexError);
             if (normalizeIndexNode != null) {
                 return normalizeIndexNode.execute(intIdx, getStoreProfile().profile(store).length());
             }
