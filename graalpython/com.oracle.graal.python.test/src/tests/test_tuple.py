928fdcd2b4dab6a338f29a489c008674304442cb
Michael Simacek <michael.simacek@oracle.com>
2020-01-08 12:55:56 +0100

    remove invalid asserts from tuple tests
    CPython 3.7 and PyPy 7.1 unify tuples as common constants, like this:
    (1, 2) is tuple(1, 2)
    CPython 3.6 and GraalPython don't do this optimization and the tests
    assume that.
    Remove this assumption as it's an implementation detail.

--- graalpython/com.oracle.graal.python.test/src/tests/test_tuple.py
+++ graalpython/com.oracle.graal.python.test/src/tests/test_tuple.py
@@ -236,12 +236,10 @@ class TupleTest(seq_tests.CommonTest):
         t = (2,)
         tt = tuple((2,))
         self.assertEqual(t,tt)
-        self.assertFalse(t is tt)
 
         ttt = tuple(t)
         self.assertEqual(t,ttt)
         self.assertEqual(tt,ttt)
-        self.assertFalse(ttt is tt)
         self.assertTrue(ttt is t)
 
         m = MyTuple((2,))
