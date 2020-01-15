8c6e57b75ebe21bc6c559aeb71619a7eb4b8d914
Michael Simacek <michael.simacek@oracle.com>
2020-01-15 16:59:26 +0100

    Fix copyrights

--- graalpython/lib-graalpython/itertools.py
+++ graalpython/lib-graalpython/itertools.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-# Copyright (c) 2017, 2019, Oracle and/or its affiliates.
+# Copyright (c) 2017, 2020, Oracle and/or its affiliates.
 # Copyright (c) 2017, The PyPy Project
 #
 #     The MIT License
