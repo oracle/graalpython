343c65e7fc77ab65e244bc107648f29e580a79ed
Tim Felgentreff <tim.felgentreff@oracle.com>
2020-01-17 09:12:36 +0100

    add a compiler assert

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/PBuiltinFunction.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/PBuiltinFunction.java
@@ -92,6 +92,7 @@ public final class PBuiltinFunction extends PythonBuiltinObject implements Bound
     }
 
     public NodeFactory<? extends PythonBuiltinBaseNode> getBuiltinNodeFactory() {
+        CompilerAsserts.neverPartOfCompilation();
         RootNode functionRootNode = getFunctionRootNode();
         if (functionRootNode instanceof BuiltinFunctionRootNode) {
             return ((BuiltinFunctionRootNode) functionRootNode).getFactory();
