ee17266f6fdbe26f05d46f4b60bf13dbe2a929b8
Michael Simacek <michael.simacek@oracle.com>
2020-01-17 14:10:23 +0100

    Avoid boxing values when constructing a tuple

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/literal/TupleLiteralNode.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/literal/TupleLiteralNode.java
@@ -25,18 +25,32 @@
  */
 package com.oracle.graal.python.nodes.literal;
 
+import java.lang.reflect.Array;
+
 import com.oracle.graal.python.builtins.objects.common.SequenceStorageNodes;
 import com.oracle.graal.python.builtins.objects.common.SequenceStorageNodes.NoGeneralizationNode;
+import com.oracle.graal.python.builtins.objects.list.PList;
 import com.oracle.graal.python.builtins.objects.tuple.PTuple;
 import com.oracle.graal.python.nodes.PNode;
 import com.oracle.graal.python.nodes.expression.ExpressionNode;
 import com.oracle.graal.python.runtime.object.PythonObjectFactory;
+import com.oracle.graal.python.runtime.sequence.storage.BoolSequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.ByteSequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.DoubleSequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.EmptySequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.IntSequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.ListSequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.LongSequenceStorage;
 import com.oracle.graal.python.runtime.sequence.storage.ObjectSequenceStorage;
 import com.oracle.graal.python.runtime.sequence.storage.SequenceStorage;
+import com.oracle.graal.python.runtime.sequence.storage.SequenceStorage.ListStorageType;
 import com.oracle.graal.python.runtime.sequence.storage.SequenceStorageFactory;
+import com.oracle.graal.python.runtime.sequence.storage.TupleSequenceStorage;
 import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.ExplodeLoop;
+import com.oracle.truffle.api.nodes.UnexpectedResultException;
 
 public final class TupleLiteralNode extends LiteralNode {
     @Child private PythonObjectFactory factory = PythonObjectFactory.create();
@@ -44,6 +58,7 @@ public final class TupleLiteralNode extends LiteralNode {
     @Child private SequenceStorageNodes.ConcatNode concatStoragesNode;
     @Child private SequenceStorageNodes.AppendNode appendNode;
     private final boolean hasStarredExpressions;
+    @CompilationFinal private ListStorageType type = ListStorageType.Uninitialized;
 
     public ExpressionNode[] getValues() {
         return values;
@@ -82,17 +97,146 @@ public final class TupleLiteralNode extends LiteralNode {
                 storage = ensureAppendNode().execute(storage, element, NoGeneralizationNode.DEFAULT);
             }
         }
+        type = storage.getElementType();
         return factory.createTuple(storage);
     }
 
     @ExplodeLoop
     private PTuple directTuple(VirtualFrame frame) {
-        final Object[] elements = new Object[values.length];
-        for (int i = 0; i < values.length; i++) {
+        SequenceStorage storage;
+        if (type == ListStorageType.Uninitialized) {
+            CompilerDirectives.transferToInterpreterAndInvalidate();
+            try {
+                Object[] elements = new Object[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    elements[i] = values[i].execute(frame);
+                }
+                storage = SequenceStorageFactory.createStorage(elements);
+                type = storage.getElementType();
+            } catch (Throwable t) {
+                // we do not want to repeatedly deopt if a value execution
+                // always raises, for example
+                type = ListStorageType.Generic;
+                throw t;
+            }
+        } else {
+            int i = 0;
+            Object array = null;
+            try {
+                switch (type) {
+                    // Ugh. We want to use primitive arrays during unpacking, so
+                    // we cannot dispatch generically here.
+                    case Empty: {
+                        assert values.length == 0;
+                        storage = EmptySequenceStorage.INSTANCE;
+                        break;
+                    }
+                    case Boolean: {
+                        boolean[] elements = new boolean[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = values[i].executeBoolean(frame);
+                        }
+                        storage = new BoolSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Byte: {
+                        byte[] elements = new byte[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            int element = values[i].executeInt(frame);
+                            if (element <= Byte.MAX_VALUE && element >= Byte.MIN_VALUE) {
+                                elements[i] = (byte) element;
+                            } else {
+                                CompilerDirectives.transferToInterpreterAndInvalidate();
+                                throw new UnexpectedResultException(element);
+                            }
+                        }
+                        storage = new ByteSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Char: {
+                        // we don't support this directly, throw and continue
+                        // generically
+                        CompilerDirectives.transferToInterpreterAndInvalidate();
+                        throw new UnexpectedResultException(values[0].execute(frame));
+                    }
+                    case Int: {
+                        int[] elements = new int[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = values[i].executeInt(frame);
+                        }
+                        storage = new IntSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Long: {
+                        long[] elements = new long[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = values[i].executeLong(frame);
+                        }
+                        storage = new LongSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Double: {
+                        double[] elements = new double[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = values[i].executeDouble(frame);
+                        }
+                        storage = new DoubleSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case List: {
+                        PList[] elements = new PList[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = PList.expect(values[i].execute(frame));
+                        }
+                        storage = new ListSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Tuple: {
+                        PTuple[] elements = new PTuple[values.length];
+                        array = elements;
+                        for (; i < values.length; i++) {
+                            elements[i] = PTuple.expect(values[i].execute(frame));
+                        }
+                        storage = new TupleSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    case Generic: {
+                        Object[] elements = new Object[values.length];
+                        for (; i < values.length; i++) {
+                            elements[i] = values[i].execute(frame);
+                        }
+                        storage = new ObjectSequenceStorage(elements, values.length);
+                        break;
+                    }
+                    default:
+                        throw new RuntimeException("unexpected state");
+                }
+            } catch (UnexpectedResultException e) {
+                CompilerDirectives.transferToInterpreterAndInvalidate();
+                storage = genericFallback(frame, array, i, e.getResult());
+            }
+        }
+        return factory.createTuple(storage);
+    }
+
+    private SequenceStorage genericFallback(VirtualFrame frame, Object array, int count, Object result) {
+        type = ListStorageType.Generic;
+        Object[] elements = new Object[values.length];
+        int i = 0;
+        for (; i < count; i++) {
+            elements[i] = Array.get(array, i);
+        }
+        elements[i++] = result;
+        for (; i < values.length; i++) {
             elements[i] = values[i].execute(frame);
         }
-        SequenceStorage storage = SequenceStorageFactory.createStorage(elements);
-        return factory.createTuple(storage);
+        return new ObjectSequenceStorage(elements, values.length);
     }
 
     private SequenceStorageNodes.ConcatNode ensureConcatStoragesNode() {
