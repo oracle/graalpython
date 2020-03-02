f6de9a8d20eda67dbba2681eaa12b39d1e70f6a4
Tim Felgentreff <tim.felgentreff@oracle.com>
2020-03-02 09:22:30 +0100

    add toDisplayString message

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/PythonLanguage.java
@@ -459,56 +459,6 @@ public final class PythonLanguage extends TruffleLanguage<PythonContext> {
         return null;
     }
 
-    @Override
-    protected String toString(PythonContext context, Object value) {
-        if (isForeignPrimitive(value)) {
-            // these only appear when inspecting foreign data structures, so they shouldn't get
-            // processed by python code
-            return value.toString();
-        }
-        if (PythonOptions.getFlag(context, PythonOptions.UseReprForPrintString)) {
-            final PythonModule builtins = context.getBuiltins();
-            if (builtins != null) {
-                // may be null during initialization
-                Object reprAttribute = builtins.getAttribute(BuiltinNames.REPR);
-                if (reprAttribute instanceof PBuiltinMethod) {
-                    // may be false if e.g. someone accessed our builtins reflectively
-                    Object reprFunction = ((PBuiltinMethod) reprAttribute).getFunction();
-                    if (reprFunction instanceof PBuiltinFunction) {
-                        // may be false if our builtins were tampered with
-                        Object[] userArgs = PArguments.create(2);
-                        PArguments.setArgument(userArgs, 0, PNone.NONE);
-                        PArguments.setArgument(userArgs, 1, value);
-                        try {
-                            Object result = InvokeNode.invokeUncached((PBuiltinFunction) reprFunction, userArgs);
-                            if (result instanceof String) {
-                                return (String) result;
-                            } else if (result instanceof PString) {
-                                return ((PString) result).getValue();
-                            } else {
-                                // This is illegal for a repr implementation, we ignore the result.
-                                // At this point it's probably difficult to report this properly.
-                            }
-                        } catch (PException e) {
-                            // Fall through to default
-                        }
-                    }
-                }
-            }
-        }
-        // This is not a good place to report inconsistencies in any of the above conditions. Just
-        // return a String
-        if (value instanceof PythonAbstractObject) {
-            return ((PythonAbstractObject) value).toString();
-        } else if (value instanceof String) {
-            return (String) value;
-        } else if (value instanceof Number) {
-            return ((Number) value).toString();
-        } else {
-            return "not a Python object";
-        }
-    }
-
     @TruffleBoundary
     public static TruffleLogger getLogger() {
         return TruffleLogger.getLogger(ID);
--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/PythonAbstractObject.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/PythonAbstractObject.java
@@ -93,6 +93,7 @@ import com.oracle.graal.python.builtins.objects.type.PythonClass;
 import com.oracle.graal.python.builtins.objects.type.PythonManagedClass;
 import com.oracle.graal.python.builtins.objects.type.TypeNodes;
 import com.oracle.graal.python.builtins.objects.type.TypeNodes.GetMroNode;
+import com.oracle.graal.python.nodes.BuiltinNames;
 import com.oracle.graal.python.nodes.PGuards;
 import com.oracle.graal.python.nodes.PRaiseNode;
 import com.oracle.graal.python.nodes.SpecialMethodNames;
@@ -116,12 +117,17 @@ import com.oracle.graal.python.nodes.object.IsBuiltinClassProfile;
 import com.oracle.graal.python.nodes.util.CastToJavaBooleanNode;
 import com.oracle.graal.python.nodes.util.CastToJavaIntNode;
 import com.oracle.graal.python.nodes.util.CastToJavaLongNode;
+import com.oracle.graal.python.nodes.util.CastToJavaStringNode;
+import com.oracle.graal.python.runtime.PythonContext;
+import com.oracle.graal.python.runtime.PythonOptions;
 import com.oracle.graal.python.runtime.exception.PException;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.TruffleLanguage;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Exclusive;
 import com.oracle.truffle.api.dsl.Cached.Shared;
+import com.oracle.truffle.api.dsl.CachedContext;
 import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.ReportPolymorphism;
 import com.oracle.truffle.api.dsl.Specialization;
@@ -1694,5 +1700,58 @@ public abstract class PythonAbstractObject implements TruffleObject, Comparable<
     @TruffleBoundary
     private static boolean objectHasAttribute(Object object, Object field) {
         return ((PythonObject) object).getAttributeNames().contains(field);
+    }
+
+    @ExportMessage
+    public boolean hasLanguage() {
+        return true;
+    }
+
+    @ExportMessage
+    public Class<? extends TruffleLanguage> getLanguage() {
+        return PythonLanguage.class;
+    }
+
+    @ExportMessage
+    public static class ToDisplayString {
+        public static final boolean useRepr(PythonContext context) {
+            return PythonOptions.getFlag(context, PythonOptions.UseReprForPrintString);
+        }
+
+        @Specialization(guards = {"allowSideEffects", "builtins != null"}) // may be null during
+                                                                           // initialization
+        public static String builtin(PythonAbstractObject self, boolean allowSideEffects,
+                        @SuppressWarnings("unused") @CachedContext(PythonLanguage.class) PythonContext context,
+                        @Cached(value = "context.getBuiltins()", allowUncached = true) PythonModule builtins,
+                        @Cached ReadAttributeFromObjectNode readStr,
+                        @Cached CallNode callNode,
+                        @Cached CastToJavaStringNode castStr,
+                        @Cached(value = "useRepr(context)", allowUncached = true) boolean useRepr) {
+            Object toStrAttr;
+            if (useRepr) {
+                toStrAttr = readStr.execute(builtins, BuiltinNames.REPR);
+            } else {
+                toStrAttr = readStr.execute(builtins, BuiltinNames.STR);
+            }
+            String result = castStr.execute(callNode.execute(toStrAttr, self));
+            if (result != null) {
+                return result;
+            } else {
+                return fallback(self, allowSideEffects, context, builtins);
+            }
+        }
+
+        public static final PythonModule none() {
+            return null;
+        }
+
+        @TruffleBoundary
+        @SuppressWarnings("unused")
+        @Specialization(guards = "!allowSideEffects || builtins == null")
+        public static String fallback(PythonAbstractObject self, boolean allowSideEffects,
+                        @CachedContext(PythonLanguage.class) PythonContext context,
+                        @Cached(value = "context.getBuiltins()", uncached = "none()") PythonModule builtins) {
+            return self.toString();
+        }
     }
 }
