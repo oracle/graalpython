b4d807a95bc9713e68368daa7f32e47620dea775
Michael Simacek <michael.simacek@oracle.com>
2020-03-02 13:23:53 +0100

    Make sure that patched pip module has __file__ set

--- graalpython/lib-graalpython/pip_hook.py
+++ graalpython/lib-graalpython/pip_hook.py
@@ -74,7 +74,9 @@ class PipImportHook:
                 real_spec = finder.find_spec(fullname, path, target=None)
                 if real_spec:
                     sys.meta_path.remove(PipImportHook)
-                    return _frozen_importlib.ModuleSpec(fullname, PipLoader(real_spec), is_package=False)
+                    spec = _frozen_importlib.ModuleSpec(fullname, PipLoader(real_spec), is_package=False, origin=real_spec.origin)
+                    spec.has_location = real_spec.has_location
+                    return spec
 
 
 sys.meta_path.insert(0, PipImportHook)
