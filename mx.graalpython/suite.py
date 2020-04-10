f4c9814d339bb351a2a51f74da905dba4ac36148
Mohamed Ez-zarghili <mohamed.ez.zarghili@oracle.com>
2020-04-10 05:41:15 +0100

    [GR-22410] Release 19.3.2.

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -10,7 +10,7 @@ suite = {
     "versionConflictResolution": "latest",
 
     "version": "19.3.2",
-    "release" : False,
+    "release" : True,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
 
