6f909cb3b5581874a48e5d85a14f1d3883f22907
Tim Felgentreff <tim.felgentreff@oracle.com>
2020-04-17 08:33:05 +0200

    fix style

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/PNodeUtil.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/PNodeUtil.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Oracle and/or its affiliates.
+ * Copyright (c) 2017, 2020, Oracle and/or its affiliates.
  * Copyright (c) 2014, Regents of the University of California
  *
  * All rights reserved.
