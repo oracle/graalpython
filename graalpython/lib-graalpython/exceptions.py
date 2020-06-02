1a7cc3e303fb5bdacf816bd30c5e85441ba7f416
Michael Simacek <michael.simacek@oracle.com>
2020-06-02 11:14:36 +0200

    Add __str__ for UnicodeError subclasses

--- graalpython/lib-graalpython/exceptions.py
+++ graalpython/lib-graalpython/exceptions.py
@@ -120,8 +120,25 @@ def UnicodeEncodeError__init__(self, encoding, object, start, end, reason):
     self.reason = reason
 
 
+def UnicodeEncodeError__str__(self):
+    if not hasattr(self, 'object'):
+        return ""
+    if self.start < len(self.object) and self.start + 1 == self.end:
+        badchar = ord(self.object[self.start])
+        if badchar <= 0xff:
+            fmt = "'%s' codec can't encode character '\\x%02x' in position %d: %s"
+        elif badchar <= 0xffff:
+            fmt = "'%s' codec can't encode character '\\u%04x' in position %d: %s"
+        else:
+            fmt = "'%s' codec can't encode character '\\U%08x' in position %d: %s"
+        return fmt % (self.encoding, badchar, self.start, self.reason)
+    return "'%s' codec can't encode characters in position %d-%d: %s" % (self.encoding, self.start, self.end - 1, self.reason)
+
+
 UnicodeEncodeError.__init__ = UnicodeEncodeError__init__
+UnicodeEncodeError.__str__ = UnicodeEncodeError__str__
 del UnicodeEncodeError__init__
+del UnicodeEncodeError__str__
 
 
 def UnicodeDecodeError__init__(self, encoding, object, start, end, reason):
@@ -135,9 +152,28 @@ def UnicodeDecodeError__init__(self, encoding, object, start, end, reason):
     self.end = end
     self.reason = reason
 
+def UnicodeEncodeError__init__(self, encoding, object, start, end, reason):
+    BaseException.__init__(self, encoding, object, start, end, reason)
+    self.encoding = encoding
+    self.object = object
+    self.start = start
+    self.end = end
+    self.reason = reason
+
+
+def UnicodeDecodeError__str__(self):
+    if not hasattr(self, 'object'):
+        return ""
+    if self.start < len(self.object) and self.start + 1 == self.end:
+        byte = self.object[self.start]
+        return "'%s' codec can't decode byte 0x%02x in position %d: %s" % (self.encoding, byte, self.start, self.reason)
+    return "'%s' codec can't decode bytes in position %d-%d: %s" % (self.encoding, self.start, self.end - 1, self.reason)
+
 
 UnicodeDecodeError.__init__ = UnicodeDecodeError__init__
+UnicodeDecodeError.__str__ = UnicodeDecodeError__str__
 del UnicodeDecodeError__init__
+del UnicodeDecodeError__str__
 
 
 def UnicodeTranslateError__init__(self, object, start, end, reason):
@@ -147,8 +183,25 @@ def UnicodeTranslateError__init__(self, object, start, end, reason):
     self.reason = reason
 
 
+def UnicodeTranslateError__str__(self):
+    if not hasattr(self, 'object'):
+        return ""
+    if self.start < len(self.object) and self.start + 1 == self.end:
+        badchar = ord(self.object[self.start])
+        if badchar <= 0xff:
+            fmt = "can't translate character '\\x%02x' in position %d: %s"
+        elif badchar <= 0xffff:
+            fmt = "can't translate character '\\u%04x' in position %d: %s"
+        else:
+            fmt = "can't translate character '\\U%08x' in position %d: %s"
+        return fmt % (badchar, self.start, self.reason)
+    return "can't translate characters in position %d-%d: %s" % (self.start, self.end - 1, self.reason)
+
+
 UnicodeTranslateError.__init__ = UnicodeTranslateError__init__
+UnicodeTranslateError.__str__ = UnicodeTranslateError__str__
 del UnicodeTranslateError__init__
+del UnicodeTranslateError__str__
 
 
 # These errors are just an alias of OSError (i.e. 'EnvironmentError is OSError == True')
