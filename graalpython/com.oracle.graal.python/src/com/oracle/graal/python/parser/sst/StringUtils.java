b45fad830e6f48c2de6fa3c27e3847837acbcfde
Petr Pisl <petr.pisl@oracle.com>
2020-06-05 11:36:34 +0200

    [GR-18284] Escaping letters in string literals using name in Unicode database

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/parser/sst/StringUtils.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/parser/sst/StringUtils.java
@@ -41,7 +41,8 @@
 
 package com.oracle.graal.python.parser.sst;
 
-import static com.oracle.graal.python.runtime.exception.PythonErrorType.SyntaxError;
+import com.oracle.graal.python.PythonLanguage;
+import com.oracle.graal.python.builtins.PythonBuiltinClassType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -53,9 +54,15 @@ import com.oracle.graal.python.nodes.control.BaseBlockNode;
 import com.oracle.graal.python.nodes.expression.ExpressionNode;
 import com.oracle.graal.python.nodes.literal.FormatStringLiteralNode;
 import com.oracle.graal.python.nodes.literal.StringLiteralNode;
+import com.oracle.graal.python.nodes.object.IsBuiltinClassProfile;
 import com.oracle.graal.python.nodes.statement.StatementNode;
 import com.oracle.graal.python.runtime.PythonParser;
+import com.oracle.graal.python.runtime.exception.PException;
 import com.oracle.graal.python.util.PythonUtils;
+import com.oracle.truffle.api.CompilerDirectives;
+import com.oracle.truffle.api.source.Source;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StringUtils {
 
@@ -108,12 +115,12 @@ public class StringUtils {
         }
     }
 
-    public static PNode parseString(String[] strings, NodeFactory nodeFactory, PythonParser.ParserErrorCallback errors) {
+    public static PNode parseString(Source source, StringLiteralSSTNode node, NodeFactory nodeFactory, PythonParser.ParserErrorCallback errors) {
         StringBuilder sb = null;
         BytesBuilder bb = null;
         boolean isFormatString = false;
         List<FormatStringLiteralNode.StringPart> formatStrings = null;
-        for (String text : strings) {
+        for (String text : node.values) {
             boolean isRaw = false;
             boolean isBytes = false;
             boolean isFormat = false;
@@ -146,7 +153,7 @@ public class StringUtils {
             text = text.substring(strStartIndex, strEndIndex);
             if (isBytes) {
                 if (sb != null || isFormatString) {
-                    throw errors.raise(SyntaxError, CANNOT_MIX_MESSAGE);
+                    throw errors.raiseInvalidSyntax(source, source.createSection(node.startOffset, node.endOffset - node.startOffset), CANNOT_MIX_MESSAGE);
                 }
                 if (bb == null) {
                     bb = new BytesBuilder();
@@ -158,10 +165,17 @@ public class StringUtils {
                 }
             } else {
                 if (bb != null) {
-                    throw errors.raise(SyntaxError, CANNOT_MIX_MESSAGE);
+                    throw errors.raiseInvalidSyntax(source, source.createSection(node.startOffset, node.endOffset - node.startOffset), CANNOT_MIX_MESSAGE);
                 }
                 if (!isRaw) {
-                    text = unescapeJavaString(text);
+                    try {
+                        text = unescapeJavaString(text);
+                    } catch (PException e) {
+                        e.expect(PythonBuiltinClassType.UnicodeDecodeError, IsBuiltinClassProfile.getUncached());
+                        String message = e.getMessage();
+                        message = "(unicode error)" + message.substring(PythonBuiltinClassType.UnicodeDecodeError.getName().length() + 1);
+                        throw errors.raiseInvalidSyntax(source, source.createSection(node.startOffset, node.endOffset - node.startOffset), message);
+                    }
                 }
                 if (isFormat) {
                     isFormatString = true;
@@ -293,6 +307,10 @@ public class StringUtils {
                         sb.append(Character.toChars(hexCode));
                         i += 3;
                         continue;
+                    case 'N':
+                        // a character from Unicode Data Database
+                        i = doCharacterName(st, sb, i + 2);
+                        continue;
                     default:
                         sb.append(ch);
                         sb.append(nextChar);
@@ -304,5 +322,177 @@ public class StringUtils {
             sb.append(ch);
         }
         return sb.toString();
+    }
+
+    private static final String UNICODE_ERROR = "'unicodeescape' codec can't decode bytes in position %d-%d:";
+    private static final String MALFORMED_ERROR = " malformed \\N character escape";
+    private static final String UNKNOWN_UNICODE_ERROR = " unknown Unicode character name";
+
+    /**
+     * Replace '/N{Unicode Character Name}' with the code point of the character. In JDK some
+     * characters use different naming. The subset is bigger in JDK 9 than in JDK 8 etc. Also the
+     * subset doesn't have to match with CPython.
+     * 
+     * @param text a text that contains /N{...} escape sequence
+     * @param sb string builder where the result code point will be written
+     * @param offset this is offset of the open brace
+     * @return offset of the close brace
+     */
+    @CompilerDirectives.TruffleBoundary
+    private static int doCharacterName(String text, StringBuilder sb, int offset) {
+        char ch = text.charAt(offset);
+        if (ch != '{') {
+            throw PythonLanguage.getCore().raise(PythonBuiltinClassType.UnicodeDecodeError, UNICODE_ERROR + MALFORMED_ERROR, offset - 2, offset - 1);
+        }
+        int closeIndex = text.indexOf("}", offset + 1);
+        if (closeIndex == -1) {
+            throw PythonLanguage.getCore().raise(PythonBuiltinClassType.UnicodeDecodeError, UNICODE_ERROR + MALFORMED_ERROR, offset - 2, text.length() - 1);
+        }
+        String charName = text.substring(offset + 1, closeIndex).toUpperCase();
+        // When JDK 1.8 will not be supported, we can replace with Character.codePointOf(String
+        // name) in the
+        int cp = CodePointNamesTable.getCodePoint(charName);
+        if (cp >= 0) {
+            sb.append(Character.toChars(cp));
+        } else {
+            throw PythonLanguage.getCore().raise(PythonBuiltinClassType.UnicodeDecodeError, UNICODE_ERROR + UNKNOWN_UNICODE_ERROR, offset - 2, closeIndex);
+        }
+        return closeIndex;
+    }
+
+    // This class class uses a cache table due supporting JDK 8. From JDK 1.9 there is
+    // Character.codePointOf​(String name) method available that can replace it.
+    private static class CodePointNamesTable {
+        private static final Map<String, Integer> NAME_MAP = new HashMap<>();
+        private static int lastCodePoint = 0;
+        private static final String BLOCK_CJK_UNIFIED_IDEOGRAPH_NAME = "CJK UNIFIED IDEOGRAPH-";
+        private static final String[][] HANGUL_SYLLABLES = new String[][]{
+                        {"G", "A", ""},
+                        {"GG", "AE", "G"},
+                        {"N", "YA", "GG"},
+                        {"D", "YAE", "GS"},
+                        {"DD", "EO", "N",},
+                        {"R", "E", "NJ"},
+                        {"M", "YEO", "NH"},
+                        {"B", "YE", "D"},
+                        {"BB", "O", "L"},
+                        {"S", "WA", "LG"},
+                        {"SS", "WAE", "LM"},
+                        {"", "OE", "LB"},
+                        {"J", "YO", "LS"},
+                        {"JJ", "U", "LT"},
+                        {"C", "WEO", "LP"},
+                        {"K", "WE", "LH"},
+                        {"T", "WI", "M"},
+                        {"P", "YU", "B"},
+                        {"H", "EU", "BS"},
+                        {null, "YI", "S"},
+                        {null, "I", "SS"},
+                        {null, null, "NG"},
+                        {null, null, "J"},
+                        {null, null, "C"},
+                        {null, null, "K"},
+                        {null, null, "T"},
+                        {null, null, "P"},
+                        {null, null, "H"}
+        };
+
+        private static int findSyllable(String partName, int maxIndex, int column) {
+            int value = -1;
+            int len = -1;
+            String part;
+            int partLen;
+            for (int i = 0; i < maxIndex; i++) {
+                part = HANGUL_SYLLABLES[i][column];
+                if (part != null) {
+                    partLen = part.length();
+                    if (partName.startsWith(part) && len < partLen) {
+                        value = i;
+                        len = partLen;
+                    }
+                }
+            }
+            return value;
+        }
+
+        public static int getCodePoint(String name) {
+            if (NAME_MAP.isEmpty()) {
+                // This is initialization of the table. Some names JDK 8 doesn't
+                // support all alliases and sequention names as CPython
+                NAME_MAP.put("LATIN CAPITAL LETTER GHA", 0x01A2);  // as LATIN CAPITAL LETTER OI.
+            }
+            if (name.startsWith("HANGUL SYLLABLE ")) {
+                // handling HANGUL SYLLABLE block
+                // java uses names composed from the name of this block and the unicode.
+                // For example HANGUL SYLLABLES B3D0 is HANGUL SYLLABLE DOLS in CPython
+                // this algorigtm is inspired from CPython
+                String part = name.substring(16); // length of 'HANGUL SYLLABLE ' string
+                int value1 = findSyllable(part, 19, 0);
+                if (value1 > -1) {
+                    part = part.substring(HANGUL_SYLLABLES[value1][0].length());
+                    int value2 = findSyllable(part, 21, 1);
+                    if (value2 > -1) {
+                        part = part.substring(HANGUL_SYLLABLES[value2][1].length());
+                        int value3 = findSyllable(part, 28, 2);
+                        if (value3 > -1 && part.length() == HANGUL_SYLLABLES[value3][2].length()) {
+                            int code = 0xAC00 + (value1 * 21 + value2) * 28 + value3;
+                            return code;
+                        }
+                    }
+                }
+            }
+            if (name.startsWith(BLOCK_CJK_UNIFIED_IDEOGRAPH_NAME)) {
+                // this block has the number in the character name
+                // Java uses names of these blocks like 'CJK Unified Ideographs Extension A 3401`
+                // etc.
+                String tNumber = name.substring(BLOCK_CJK_UNIFIED_IDEOGRAPH_NAME.length());
+                try {
+                    int number = Integer.parseInt(tNumber, 16);
+                    if ((0x3400 <= number && number <= 0x4DBF) // CJK Unified Ideographs Extension A
+                                    || (0x4E00 <= number && number <= 0x9FFF) // CJK Unified
+                                                                              // Ideographs
+                                    || (0x20000 <= number && number <= 0x2A6DF) // CJK Unified
+                                                                                // Ideographs
+                                                                                // Extension B
+                                    || (0x2A700 <= number && number <= 0x2B73F) // CJK Unified
+                                                                                // Ideographs
+                                                                                // Extension C
+                                    || (0x2B740 <= number && number <= 0x2B81F) // CJK Unified
+                                                                                // Ideographs
+                                                                                // Extension D
+                                    || (0x2B820 <= number && number <= 0x2CEAF) // CJK Unified
+                                                                                // Ideographs
+                                                                                // Extension E
+                                    || (0x2CEB0 <= number && number <= 0x2EBEF) // CJK Unified
+                                                                                // Ideographs
+                                                                                // Extension F
+                    ) {
+                        return number;
+                    }
+                } catch (NumberFormatException e) {
+                    // do nothing
+                }
+            }
+            Integer possibleChar = NAME_MAP.get(name);
+            if (possibleChar == null && lastCodePoint < Character.MAX_CODE_POINT) {
+                // fill the table with jdk names
+                while (lastCodePoint < Character.MAX_CODE_POINT) {
+                    String charName = Character.getName(lastCodePoint);
+                    if (charName != null) {
+                        NAME_MAP.put(charName, lastCodePoint);
+                        if (name.equals(charName)) {
+                            lastCodePoint++;
+                            return lastCodePoint - 1;
+                        }
+                    }
+                    lastCodePoint++;
+                }
+            }
+            if (possibleChar != null) {
+                return possibleChar;
+            }
+
+            return -1;
+        }
     }
 }
