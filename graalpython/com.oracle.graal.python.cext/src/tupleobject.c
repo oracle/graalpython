7e1921747e4031c1126a2fe35a7c3bc08a72d0cf
Tim Felgentreff <tim.felgentreff@oracle.com>
2020-07-22 10:59:14 +0200

    update PyTuple_Pack to use va_list

--- graalpython/com.oracle.graal.python.cext/src/tupleobject.c
+++ graalpython/com.oracle.graal.python.cext/src/tupleobject.c
@@ -77,15 +77,19 @@ PyObject* PyTuple_GetSlice(PyObject *tuple, Py_ssize_t i, Py_ssize_t j) {
 
 NO_INLINE
 PyObject* PyTuple_Pack(Py_ssize_t n, ...) {
+    va_list vargs;
+    va_start(vargs, n);
     PyObject *result = PyTuple_New(n);
     if (result == NULL) {
-        return NULL;
+        goto end;
     }
     for (int i = 0; i < n; i++) {
-        PyObject *o = polyglot_get_arg(i+1);
+        PyObject *o = va_arg(vargs, PyObject *);
         Py_XINCREF(o);
         PyTuple_SetItem(result, i, o);
     }
+ end:
+    va_end(vargs);
     return result;
 }
 
