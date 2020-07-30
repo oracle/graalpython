23a35d98f7d35ceccf39f97fdc79960d9af7c46a
Florian Angerer <florian.angerer@oracle.com>
2020-07-30 19:32:55 +0200

    Add non-small int test arg to test_misc.py::PointerEquality_Primtive.

--- graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_misc.py
+++ graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_misc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2020, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -194,6 +194,7 @@ class TestMisc(CPyExtTestCase):
         lambda: (
             (True, lambda arg0, *args: arg0),
             (False, lambda arg0, *args: arg0),
+            (1000, lambda arg0, *args: arg0),
             (10, lambda arg0, *args: arg0),
             (10.0, lambda arg0, *args: arg0),
             (float('nan'), lambda arg0, *args: arg0),
