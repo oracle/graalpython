2886893b11e8b5a334ff31f23484aa36088bf7cb
Michael Simacek <michael.simacek@oracle.com>
2020-08-03 13:25:02 +0200

    Add more tests for yield expression resuming

--- graalpython/com.oracle.graal.python.test/src/com/oracle/graal/python/test/generator/YieldExpressionTests.java
+++ graalpython/com.oracle.graal.python.test/src/com/oracle/graal/python/test/generator/YieldExpressionTests.java
@@ -1,3 +1,43 @@
+/*
+ * Copyright (c) 2020, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * The Universal Permissive License (UPL), Version 1.0
+ *
+ * Subject to the condition set forth below, permission is hereby granted to any
+ * person obtaining a copy of this software, associated documentation and/or
+ * data (collectively the "Software"), free of charge and under any and all
+ * copyright rights in the Software, and any and all patent rights owned or
+ * freely licensable by each licensor hereunder covering either (i) the
+ * unmodified Software as contributed to or provided by such licensor, or (ii)
+ * the Larger Works (as defined below), to deal in both
+ *
+ * (a) the Software, and
+ *
+ * (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+ * one is included with the Software each a "Larger Work" to which the Software
+ * is contributed by such licensors),
+ *
+ * without restriction, including without limitation the rights to copy, create
+ * derivative works of, display, perform, and distribute the Software and make,
+ * use, sell, offer for sale, import, export, have made, and have sold the
+ * Software and the Larger Work(s), and to sublicense the foregoing rights on
+ * either these or other terms.
+ *
+ * This license is subject to the following condition:
+ *
+ * The above copyright notice and either this complete permission notice or at a
+ * minimum a reference to the UPL must be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.oracle.graal.python.test.generator;
 
 import org.junit.Test;
@@ -176,6 +216,55 @@ public class YieldExpressionTests {
     @Test
     public void testYieldExprCall() {
         String source = "list((lambda: dict({'a': print('x')}, b=(yield), c=print('y'), d=(yield)))())";
+        PythonTests.assertPrints("x\ny\n", source);
+    }
+
+    @Test
+    public void testYieldExprDef() {
+        String source = "def gen():\n" +
+                        "  def inner(a=print('x'), b=(yield), c=print('y')):\n" +
+                        "    pass\n" +
+                        "list(gen())";
+        PythonTests.assertPrints("x\ny\n", source);
+    }
+
+    @Test
+    public void testYieldExprSlice() {
+        String source = "list((lambda: [][print('x') or 1:(yield) or 1:print('y') or 1])())";
+        PythonTests.assertPrints("x\ny\n", source);
+    }
+
+    @Test
+    public void testYieldExprTernaryIf1() {
+        String source = "list((lambda: (yield) if print('x') else print('y'))())";
+        PythonTests.assertPrints("x\ny\n", source);
+    }
+
+    @Test
+    public void testYieldExprTernaryIf2() {
+        String source = "list((lambda: print('y') if print('x') else (yield))())";
+        PythonTests.assertPrints("x\n", source);
+    }
+
+    @Test
+    public void testYieldExprWith() {
+        String source = "class cm:\n" +
+                        "  def __init__(self):\n" +
+                        "    print('init')\n" +
+                        "  def __enter__(self, *args):\n" +
+                        "    print('enter')\n" +
+                        "  def __exit__(self, *args):\n" +
+                        "    print('exit')\n" +
+                        "def gen():\n" +
+                        "  with cm() as x, ((yield) or cm()) as y:\n" +
+                        "    pass\n" +
+                        "list(gen())";
+        PythonTests.assertPrints("init\nenter\ninit\nenter\nexit\nexit\n", source);
+    }
+
+    @Test
+    public void testYieldExprFString() {
+        String source = "list((lambda: f\"{print('x')}{(yield)}{print('y')}\")())";
         PythonTests.assertPrints("x\ny\n", source);
     }
 }
