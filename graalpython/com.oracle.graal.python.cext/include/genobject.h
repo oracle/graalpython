2893d5b2f88733fa562978407c8b5a83cde14866
Florian Angerer <florian.angerer@oracle.com>
2020-08-13 20:21:06 +0200

    Add license header to genobject.h and tracemalloc.h

--- graalpython/com.oracle.graal.python.cext/include/genobject.h
+++ graalpython/com.oracle.graal.python.cext/include/genobject.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2020, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2017 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
 
 /* Generator object interface */
 
--- graalpython/com.oracle.graal.python.cext/include/tracemalloc.h
+++ graalpython/com.oracle.graal.python.cext/include/tracemalloc.h
@@ -1,3 +1,9 @@
+/* Copyright (c) 2020, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2017 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
+
 #ifndef Py_TRACEMALLOC_H
 #define Py_TRACEMALLOC_H
 
