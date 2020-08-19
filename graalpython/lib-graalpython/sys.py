3f29d8720900c5baca8487cbb36d0e4b52c00992
Cosmin Basca <cosmin.basca@oracle.com>
2020-08-19 15:39:35 +0200

    move sys.__graalpython_flags__ to __graalpython__.flags

--- graalpython/lib-graalpython/sys.py
+++ graalpython/lib-graalpython/sys.py
@@ -72,7 +72,7 @@ def make_flags_class():
 
     def make_func(i):
         def func(self):
-            return __graalpython_flags__[i]
+            return __graalpython__.flags[i]
         return func
 
     for i, f in enumerate(names):
