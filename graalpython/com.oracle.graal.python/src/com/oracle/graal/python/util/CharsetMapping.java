875e449cc93fc8ebb76c3e91be0df1f8ac60c1e2
Michael Simacek <michael.simacek@oracle.com>
2020-09-21 13:07:36 +0200

    Synchronize lazy loading charsets

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/util/CharsetMapping.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/util/CharsetMapping.java
@@ -46,6 +46,8 @@ import java.nio.charset.UnsupportedCharsetException;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 import com.ibm.icu.charset.CharsetICU;
 import com.oracle.graal.python.charset.PythonRawUnicodeEscapeCharset;
@@ -56,7 +58,8 @@ import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
  * Utility class for mapping Python encodings to Java charsets
  */
 public class CharsetMapping {
-    private static final Map<String, Charset> JAVA_CHARSETS = new HashMap<>();
+    private static final ConcurrentMap<String, Charset> JAVA_CHARSETS = new ConcurrentHashMap<>();
+    // Name maps are populated by static initializer and are immutable afterwards
     private static final Map<String, String> CHARSET_NAME_MAP = new HashMap<>();
     private static final Map<String, String> CHARSET_NAME_MAP_REVERSE = new HashMap<>();
 
@@ -80,26 +83,23 @@ public class CharsetMapping {
     }
 
     private static Charset getJavaCharset(String name) {
-        Charset charset = JAVA_CHARSETS.get(name);
-        if (charset == null) {
+        return JAVA_CHARSETS.computeIfAbsent(name, key -> {
             // Important: When adding additional ICU4J charset, the implementation class needs to be
             // added to reflect-config.json
             if (name.equals("UTF-7") || name.equals("HZ")) {
                 try {
-                    charset = CharsetICU.forNameICU(name);
+                    return CharsetICU.forNameICU(name);
                 } catch (UnsupportedCharsetException e) {
-                    // Let it stay null
+                    return null;
                 }
             } else {
                 try {
-                    charset = Charset.forName(name);
+                    return Charset.forName(name);
                 } catch (UnsupportedCharsetException e) {
-                    // Let it stay null
+                    return null;
                 }
             }
-            JAVA_CHARSETS.put(name, charset);
-        }
-        return charset;
+        });
     }
 
     private static void addMapping(String pythonName, String javaName) {
