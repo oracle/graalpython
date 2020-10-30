58d72a91245f3ad4e22fe4cf6050a2f238a2ab9f
Cosmin Basca <cosmin.basca@oracle.com>
2020-10-30 11:29:02 +0100

    PNotImplemented: add __reduce__ and __repr__ builtin implementations

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/Python3Core.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/Python3Core.java
@@ -44,6 +44,7 @@ import java.util.logging.Level;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import com.oracle.graal.python.builtins.objects.NotImplementedBuiltins;
 import com.oracle.graal.python.builtins.objects.ellipsis.EllipsisBuiltins;
 import org.graalvm.nativeimage.ImageInfo;
 
@@ -341,6 +342,7 @@ public final class Python3Core implements PythonCore {
                         new PZipBuiltins(),
                         new EnumerateBuiltins(),
                         new MapBuiltins(),
+                        new NotImplementedBuiltins(),
                         new EllipsisBuiltins(),
                         new SentinelIteratorBuiltins(),
                         new ForeignIteratorBuiltins(),
