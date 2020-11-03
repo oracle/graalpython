4d768e273d69e7dea2d66ff3185bbfdf72cca0b6
Tim Felgentreff <tim.felgentreff@oracle.com>
2020-11-03 07:14:21 +0100

    [GR-9150] sysconfig and platform now work

--- graalpython/lib-python/3/test/support/__init__.py
+++ graalpython/lib-python/3/test/support/__init__.py
@@ -2163,11 +2163,9 @@ def _check_docstrings():
     """Just used to check if docstrings are enabled"""
 
 
-#TODO reenable me once platform and sysconfig are implemented/supported
-# MISSING_C_DOCSTRINGS = (check_impl_detail() and
-#                         sys.platform != 'win32' and
-#                         not sysconfig.get_config_var('WITH_DOC_STRINGS'))
-MISSING_C_DOCSTRINGS = False
+MISSING_C_DOCSTRINGS = (check_impl_detail() and
+                        sys.platform != 'win32' and
+                        not sysconfig.get_config_var('WITH_DOC_STRINGS'))
 
 HAVE_DOCSTRINGS = (_check_docstrings.__doc__ is not None and
                    not MISSING_C_DOCSTRINGS)
