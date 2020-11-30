9272e8e6e065aacef8fd033c772d4639a539a30e
Michael Simacek <michael.simacek@oracle.com>
2020-11-30 13:28:03 +0100

    Add our own installation scheme to sysconfig

--- graalpython/lib-python/3/test/test_sysconfig.py
+++ graalpython/lib-python/3/test/test_sysconfig.py
@@ -228,7 +228,8 @@ class TestSysConfig(unittest.TestCase):
         self.assertTrue(os.path.isfile(config_h), config_h)
 
     def test_get_scheme_names(self):
-        wanted = ('nt', 'nt_user', 'osx_framework_user',
+        # XXX Graalpython change: add our scheme
+        wanted = ('graalpython', 'nt', 'nt_user', 'osx_framework_user',
                   'posix_home', 'posix_prefix', 'posix_user')
         self.assertEqual(get_scheme_names(), wanted)
 
