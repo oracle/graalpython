ebc36d956858002faab4b27333829531cde1c46d
Tomas Stupka <tomas.stupka@oracle.com>
2021-01-06 18:43:03 +0100

    updated _functools.reduce

--- graalpython/lib-graalpython/_functools.py
+++ graalpython/lib-graalpython/_functools.py
@@ -66,16 +66,24 @@ def recursive_repr(fillvalue='...'):
 
     return decorating_function
 
+_initial_missing = object()
 @__graalpython__.builtin
-def reduce(function, iterable, initializer=None):
-    it = iter(iterable)
-    if initializer is None:
-        value = next(it)
+def reduce(function, sequence, initial=_initial_missing):
+    it = iter(sequence)
+
+    if initial is _initial_missing:
+        try:
+            value = next(it)
+        except StopIteration:
+            raise TypeError("reduce() of empty sequence with no initial value") from None
     else:
-        value = initializer
+        value = initial
+
     for element in it:
         value = function(value, element)
+
     return value
+
 
 @__graalpython__.builtin
 def cmp_to_key(mycmp):
