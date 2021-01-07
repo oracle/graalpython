7240bc536c6b3914c971a4fee3f785b53635e28b
Michael Simacek <michael.simacek@oracle.com>
2021-01-07 12:49:55 +0100

    Fix invalid group reference error message

--- graalpython/lib-graalpython/_sre.py
+++ graalpython/lib-graalpython/_sre.py
@@ -391,7 +391,7 @@ class SRE_Pattern():
                     group_nr = int(c)
                     group_str = group(pattern, match_result, group_nr, string)
                     if group_str is None:
-                        raise error("invalid group reference %s at position %s" % (group_nr, pos))
+                        raise error("invalid group reference %s" % group_nr)
                     result += repl[start:pos] + group_str
                     start = pos + 2
                 elif c == 'g':
@@ -399,7 +399,7 @@ class SRE_Pattern():
                     if group_ref:
                         group_str = group(pattern, match_result, int(group_ref) if digits_only else pattern.groups[group_ref], string)
                         if group_str is None:
-                            raise error("invalid group reference %s at position %s" % (group_ref, pos))
+                            raise error("invalid group reference %s" % group_ref)
                         result += repl[start:pos] + group_str
                     start = group_ref_end + 1
                 elif c == '\\':
