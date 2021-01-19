03daf09a327f81368def379438f7ece41153dc0e
Florian Angerer <florian.angerer@oracle.com>
2021-01-19 13:57:21 +0100

    Update imports

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -44,7 +44,7 @@ suite = {
             },
             {
                 "name": "tools",
-                "version": "90e1de37453563f76f5e1ae91326393017064b62",
+                "version": "21a3c29d9b142a6005b4fe9bd5c0935cbdb28105",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
@@ -52,7 +52,7 @@ suite = {
             },
             {
                 "name": "sulong",
-                "version": "90e1de37453563f76f5e1ae91326393017064b62",
+                "version": "21a3c29d9b142a6005b4fe9bd5c0935cbdb28105",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
@@ -60,7 +60,7 @@ suite = {
             },
             {
                 "name": "regex",
-                "version": "90e1de37453563f76f5e1ae91326393017064b62",
+                "version": "21a3c29d9b142a6005b4fe9bd5c0935cbdb28105",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
