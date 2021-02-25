55a3e253b9d193bf1b4dcf1675369246d8ffa494
Michael Simacek <michael.simacek@oracle.com>
2021-02-25 09:49:52 +0100

    Fix checkstyle suppression

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/util/IPAddressUtil.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/util/IPAddressUtil.java
@@ -38,7 +38,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-// CHECKSTYLE:OFF
+// Checkstyle: stop
 package com.oracle.graal.python.util;
 
 import java.net.URL;
