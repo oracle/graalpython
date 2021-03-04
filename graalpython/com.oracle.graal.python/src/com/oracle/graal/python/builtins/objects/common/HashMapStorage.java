17a87e3827e4df08108581c5c554566c2fc49158
Cosmin Basca <cosmin.basca@oracle.com>
2021-03-04 13:01:24 +0100

    add GIL: HashMapStorage

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/HashMapStorage.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/HashMapStorage.java
@@ -53,9 +53,11 @@ import com.oracle.graal.python.builtins.objects.object.PythonObjectLibrary;
 import com.oracle.graal.python.nodes.PGuards;
 import com.oracle.graal.python.nodes.object.IsBuiltinClassProfile;
 import com.oracle.graal.python.nodes.util.CastToJavaStringNode;
+import com.oracle.graal.python.runtime.GilNode;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
 import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.Cached.Exclusive;
+import com.oracle.truffle.api.dsl.Cached.Shared;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.library.CachedLibrary;
 import com.oracle.truffle.api.library.ExportLibrary;
@@ -154,10 +156,14 @@ public class HashMapStorage extends HashingStorage {
         }
     }
 
-    @Override
     @ExportMessage
-    public int length() {
-        return size(values);
+    public int length(@Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            return size(values);
+        } finally {
+            gil.release(mustRelease);
+        }
     }
 
     @TruffleBoundary
@@ -168,15 +174,25 @@ public class HashMapStorage extends HashingStorage {
     @ExportMessage
     static class GetItemWithState {
         @Specialization
-        static Object getItemString(HashMapStorage self, String key, @SuppressWarnings("unused") ThreadState state) {
-            return get(self.values, key);
+        static Object getItemString(HashMapStorage self, String key, @SuppressWarnings("unused") ThreadState state, @Shared("gil") @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                return get(self.values, key);
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @Specialization(replaces = "getItemString", guards = "isSupportedKey(key, profile)")
         static Object getItem(HashMapStorage self, Object key, @SuppressWarnings("unused") ThreadState state,
                         @Cached CastToJavaStringNode castNode,
-                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile) {
-            return get(self.values, castNode.execute(key));
+                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                return get(self.values, castNode.execute(key));
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @TruffleBoundary
@@ -188,34 +204,49 @@ public class HashMapStorage extends HashingStorage {
         static Object getItemNotSupportedKey(@SuppressWarnings("unused") HashMapStorage self, Object key, @SuppressWarnings("unused") ThreadState state,
                         @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile,
                         @CachedLibrary("key") PythonObjectLibrary lib,
-                        @Exclusive @Cached("createBinaryProfile()") ConditionProfile gotState) {
-            // we must still search the map for items that may have the same hash and that may
-            // return true from key.__eq__, we use artificial object with overridden Java level
-            // equals and hashCode methods to perform this search
-            long hash = getHashWithState(key, lib, state, gotState);
-            if (PInt.isIntRange(hash)) {
-                CustomKey keyObj = new CustomKey(key, (int) hash, state);
-                return get(self.values, keyObj);
+                        @Exclusive @Cached("createBinaryProfile()") ConditionProfile gotState, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                // we must still search the map for items that may have the same hash and that may
+                // return true from key.__eq__, we use artificial object with overridden Java level
+                // equals and hashCode methods to perform this search
+                long hash = getHashWithState(key, lib, state, gotState);
+                if (PInt.isIntRange(hash)) {
+                    CustomKey keyObj = new CustomKey(key, (int) hash, state);
+                    return get(self.values, keyObj);
+                }
+                // else the hashes cannot possibly match
+                return null;
+            } finally {
+                gil.release(mustRelease);
             }
-            // else the hashes cannot possibly match
-            return null;
         }
     }
 
     @ExportMessage
     static class SetItemWithState {
         @Specialization
-        static HashingStorage setItemString(HashMapStorage self, String key, Object value, @SuppressWarnings("unused") ThreadState state) {
-            put(self.values, key, value);
-            return self;
+        static HashingStorage setItemString(HashMapStorage self, String key, Object value, @SuppressWarnings("unused") ThreadState state, @Shared("gil") @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                put(self.values, key, value);
+                return self;
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @Specialization(replaces = "setItemString", guards = "isSupportedKey(key, profile)")
         static HashingStorage setItem(HashMapStorage self, Object key, Object value, @SuppressWarnings("unused") ThreadState state,
                         @Cached CastToJavaStringNode castNode,
-                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile) {
-            put(self.values, castNode.execute(key), value);
-            return self;
+                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                put(self.values, castNode.execute(key), value);
+                return self;
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @TruffleBoundary
@@ -227,28 +258,43 @@ public class HashMapStorage extends HashingStorage {
         static HashingStorage setItemNotSupportedKey(HashMapStorage self, Object key, Object value, @SuppressWarnings("unused") ThreadState state,
                         @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile,
                         @CachedLibrary("self") HashingStorageLibrary thisLib,
-                        @CachedLibrary(limit = "1") HashingStorageLibrary newLib) {
-            HashingStorage newStore = EconomicMapStorage.create(self.length());
-            thisLib.addAllToOther(self, newStore);
-            newLib.setItem(newStore, key, value);
-            return newStore;
+                        @CachedLibrary(limit = "1") HashingStorageLibrary newLib, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                HashingStorage newStore = EconomicMapStorage.create(self.length());
+                thisLib.addAllToOther(self, newStore);
+                newLib.setItem(newStore, key, value);
+                return newStore;
+            } finally {
+                gil.release(mustRelease);
+            }
         }
     }
 
     @ExportMessage
     static class DelItemWithState {
         @Specialization
-        static HashingStorage delItemString(HashMapStorage self, String key, @SuppressWarnings("unused") ThreadState state) {
-            remove(self.values, key);
-            return self;
+        static HashingStorage delItemString(HashMapStorage self, String key, @SuppressWarnings("unused") ThreadState state, @Shared("gil") @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                remove(self.values, key);
+                return self;
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @Specialization(replaces = "delItemString", guards = "isSupportedKey(key, profile)")
         static HashingStorage delItem(HashMapStorage self, Object key, @SuppressWarnings("unused") ThreadState state,
                         @Cached CastToJavaStringNode castNode,
-                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile) {
-            remove(self.values, castNode.execute(key));
-            return self;
+                        @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                remove(self.values, castNode.execute(key));
+                return self;
+            } finally {
+                gil.release(mustRelease);
+            }
         }
 
         @TruffleBoundary
@@ -260,35 +306,48 @@ public class HashMapStorage extends HashingStorage {
         static HashingStorage delItemNonSupportedKey(HashMapStorage self, @SuppressWarnings("unused") Object key, @SuppressWarnings("unused") ThreadState state,
                         @SuppressWarnings("unused") @Cached IsBuiltinClassProfile profile,
                         @CachedLibrary("key") PythonObjectLibrary lib,
-                        @Exclusive @Cached("createBinaryProfile()") ConditionProfile gotState) {
-            // we must still search the map for items that may have the same hash and that may
-            // return true from key.__eq__, we use artificial object with overridden Java level
-            // equals and hashCode methods to perform this search
-            long hash = getHashWithState(key, lib, state, gotState);
-            if (PInt.isIntRange(hash)) {
-                CustomKey keyObj = new CustomKey(key, (int) hash, state);
-                remove(self.values, keyObj);
+                        @Exclusive @Cached("createBinaryProfile()") ConditionProfile gotState, @Cached GilNode gil) {
+            boolean mustRelease = gil.acquire();
+            try {
+                // we must still search the map for items that may have the same hash and that may
+                // return true from key.__eq__, we use artificial object with overridden Java level
+                // equals and hashCode methods to perform this search
+                long hash = getHashWithState(key, lib, state, gotState);
+                if (PInt.isIntRange(hash)) {
+                    CustomKey keyObj = new CustomKey(key, (int) hash, state);
+                    remove(self.values, keyObj);
+                }
+                // else the hashes cannot possibly match
+                return self;
+            } finally {
+                gil.release(mustRelease);
             }
-            // else the hashes cannot possibly match
-            return self;
         }
     }
 
-    @Override
     @ExportMessage
-    Object forEachUntyped(ForEachNode<Object> node, Object argIn) {
-        Object arg = argIn;
-        for (Object key : keys()) {
-            arg = node.execute(key, arg);
+    Object forEachUntyped(ForEachNode<Object> node, Object argIn, @Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            Object arg = argIn;
+            for (Object key : keys()) {
+                arg = node.execute(key, arg);
+            }
+            return arg;
+        } finally {
+            gil.release(mustRelease);
         }
-        return arg;
     }
 
-    @Override
     @ExportMessage
-    public HashingStorage clear() {
-        clearMap(values);
-        return this;
+    public HashingStorage clear(@Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            clearMap(values);
+            return this;
+        } finally {
+            gil.release(mustRelease);
+        }
     }
 
     @TruffleBoundary
@@ -296,16 +355,24 @@ public class HashMapStorage extends HashingStorage {
         map.clear();
     }
 
-    @Override
     @ExportMessage
-    public HashingStorage copy() {
-        return new HashMapStorage(newHashMap(values));
+    public HashingStorage copy(@Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            return new HashMapStorage(newHashMap(values));
+        } finally {
+            gil.release(mustRelease);
+        }
     }
 
-    @Override
     @ExportMessage
-    public HashingStorageIterable<Object> keys() {
-        return new HashingStorageIterable<>(getKeysIterator(values));
+    public HashingStorageIterable<Object> keys(@Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            return new HashingStorageIterable<>(getKeysIterator(values));
+        } finally {
+            gil.release(mustRelease);
+        }
     }
 
     private static Iterator<Object> getKeysIterator(LinkedHashMap<Object, Object> map) {
@@ -313,9 +380,13 @@ public class HashMapStorage extends HashingStorage {
     }
 
     @ExportMessage
-    @Override
-    public HashingStorageIterable<Object> reverseKeys() {
-        return new HashingStorageIterable<>(getReverseIterator(values));
+    public HashingStorageIterable<Object> reverseKeys(@Shared("gil") @Cached GilNode gil) {
+        boolean mustRelease = gil.acquire();
+        try {
+            return new HashingStorageIterable<>(getReverseIterator(values));
+        } finally {
+            gil.release(mustRelease);
+        }
     }
 
     @TruffleBoundary
