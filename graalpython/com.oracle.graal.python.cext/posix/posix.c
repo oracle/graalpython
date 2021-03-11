93c72a2cb61f2ad90b6e5f8bc05ab3ee185ec4da
Ondrej Tethal <ondrej.tethal@oracle.com>
2021-03-11 10:54:09 +0100

    Treat warnings as errors during posix support so build, added missing include for kill()

--- graalpython/com.oracle.graal.python.cext/posix/posix.c
+++ graalpython/com.oracle.graal.python.cext/posix/posix.c
@@ -51,6 +51,7 @@
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
--- graalpython/com.oracle.graal.python.cext/setup.py
+++ graalpython/com.oracle.graal.python.cext/setup.py
@@ -443,7 +443,7 @@ def build_libposix(capi_home):
     files = [os.path.abspath(os.path.join(src_dir, f)) for f in os.listdir(src_dir) if f.endswith(".c")]
     module = Extension(libposix_name,
                        sources=files,
-                       extra_compile_args=cflags_warnings)
+                       extra_compile_args=cflags_warnings + ['-Wall', '-Werror'])
     args = [verbosity, 'build', 'install_lib', '-f', '--install-dir=%s' % capi_home, "clean"]
     setup(
         script_name='setup' + libposix_name,
