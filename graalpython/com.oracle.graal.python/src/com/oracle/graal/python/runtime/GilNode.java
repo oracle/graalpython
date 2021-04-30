4dfa0eb1cc9518f9fc72719b23b0428f7819288b
Tim Felgentreff <tim.felgentreff@oracle.com>
2021-04-30 13:54:09 +0200

    fix style

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/GilNode.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/GilNode.java
@@ -42,7 +42,6 @@
 package com.oracle.graal.python.runtime;
 
 import com.oracle.graal.python.PythonLanguage;
-import com.oracle.graal.python.runtime.PythonContext.PythonThreadState;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.CompilerDirectives.CompilationFinal;
 import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
