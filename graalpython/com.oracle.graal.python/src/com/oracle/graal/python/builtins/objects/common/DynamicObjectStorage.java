1874dfa1bc03273f35416fa6875ba41fc31a0f04
Michael Simacek <michael.simacek@oracle.com>
2021-10-15 19:44:25 +0200

    Fix clearing storage of __dict__

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/DynamicObjectStorage.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/common/DynamicObjectStorage.java
@@ -48,8 +48,10 @@ import com.oracle.graal.python.PythonLanguage;
 import com.oracle.graal.python.builtins.objects.PNone;
 import com.oracle.graal.python.builtins.objects.common.HashingStorageLibrary.ForEachNode;
 import com.oracle.graal.python.builtins.objects.common.HashingStorageLibrary.HashingStorageIterable;
+import com.oracle.graal.python.builtins.objects.dict.PDict;
 import com.oracle.graal.python.builtins.objects.function.PArguments;
 import com.oracle.graal.python.builtins.objects.function.PArguments.ThreadState;
+import com.oracle.graal.python.builtins.objects.object.PythonObject;
 import com.oracle.graal.python.builtins.objects.str.PString;
 import com.oracle.graal.python.lib.PyObjectHashNode;
 import com.oracle.graal.python.lib.PyObjectRichCompareBool;
@@ -394,9 +396,30 @@ public final class DynamicObjectStorage extends HashingStorage {
     }
 
     @ExportMessage
-    public HashingStorage clear(@Shared("dylib") @CachedLibrary(limit = "3") DynamicObjectLibrary dylib) {
-        dylib.resetShape(store, PythonLanguage.get(dylib).getEmptyShape());
-        return this;
+    @ImportStatic(PGuards.class)
+    static class Clear {
+        @Specialization(guards = "!isPythonObject(receiver.getStore())")
+        static HashingStorage clearPlain(DynamicObjectStorage receiver,
+                        @Shared("dylib") @CachedLibrary(limit = "3") DynamicObjectLibrary dylib) {
+            dylib.resetShape(receiver.getStore(), PythonLanguage.get(dylib).getEmptyShape());
+            return receiver;
+        }
+
+        @Specialization(guards = "isPythonObject(receiver.getStore())")
+        static HashingStorage clearObjectBacked(DynamicObjectStorage receiver,
+                        @Shared("dylib") @CachedLibrary(limit = "3") DynamicObjectLibrary dylib) {
+            /*
+             * We cannot use resetShape as that would lose hidden keys, such as CLASS or OBJ_ID.
+             * Construct a new storage instead and set it as the object's __dict__'s storage.
+             */
+            DynamicObjectStorage newStorage = new DynamicObjectStorage(new Store(PythonLanguage.get(dylib).getEmptyShape()));
+            PythonObject owner = (PythonObject) receiver.getStore();
+            PDict dict = (PDict) dylib.getOrDefault(owner, PythonObject.DICT, null);
+            if (dict != null && dict.getDictStorage() == receiver) {
+                dict.setDictStorage(newStorage);
+            }
+            return newStorage;
+        }
     }
 
     @ExportMessage
