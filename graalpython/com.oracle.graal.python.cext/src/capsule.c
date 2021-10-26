3b08b0b5134f75f0f5e8b937dfb88b457d1ae30e
Michael Simacek <michael.simacek@oracle.com>
2021-10-26 19:41:06 +0200

    Add copyright to capsule.c

--- graalpython/com.oracle.graal.python.cext/src/capsule.c
+++ graalpython/com.oracle.graal.python.cext/src/capsule.c
@@ -1,3 +1,8 @@
+/* Copyright (c) 2021, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2021 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
 /* Wrap void * pointers to be passed between C modules */
 
 #include "Python.h"
