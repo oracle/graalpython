61038869e3cc5d77196f948fc416f18c1c3fc18f
Tomas Stupka <tomas.stupka@oracle.com>
2022-01-13 22:10:27 +0100

    fixed imports and license dates

--- graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_functions.py
+++ graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_functions.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -72,9 +72,9 @@ class TestPyObject(CPyExtTestCase):
         type,
         lambda: ([], 12, sys.modules)
     )
-    
+
     # Below are the PyObject_* identifiers that we know are used in numpy
-    
+
     def is_buffer(x):
         __breakpoint__()
         if (isinstance(x, bytes) or isinstance(x, bytearray) or isinstance(x, array.array)):
@@ -184,7 +184,7 @@ class TestPyObject(CPyExtTestCase):
         argspec="Os",
         callfunction="PyObject_CallFunction",
     )
-    
+
     class MyObject():
 
         def foo(self, *args, **kwargs):
