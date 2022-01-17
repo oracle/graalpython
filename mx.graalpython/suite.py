49fb3cd11495ecb0a71512a2785bca806c48b113
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2022-01-17 11:16:52 +0100

    GraalVM 22.0.0.1 release

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "22.0.0",
+    "version": "22.0.0.1",
     "release": True,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
