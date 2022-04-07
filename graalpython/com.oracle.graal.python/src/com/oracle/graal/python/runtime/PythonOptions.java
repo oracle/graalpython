3d5d805dd4789876d02c6de5083f2ed478d4391e
Michael Simacek <michael.simacek@oracle.com>
2022-04-07 17:54:57 +0200

    Allow disabling frozen modules

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/PythonOptions.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/runtime/PythonOptions.java
@@ -289,6 +289,9 @@ public final class PythonOptions {
     @Option(category = OptionCategory.EXPERT, help = "Enable built-in functions on the __graalpython__ module that are useful for debugging.") //
     public static final OptionKey<Boolean> EnableDebuggingBuiltins = new OptionKey<>(false);
 
+    @Option(category = OptionCategory.EXPERT, help = "Disables using frozen modules.") //
+    public static final OptionKey<Boolean> DisableFrozenModules = new OptionKey<>(false);
+
     @Option(category = OptionCategory.EXPERT, help = "Enables using experimental bytecode interpreter instead of AST interpreter.") //
     public static final OptionKey<Boolean> EnableBytecodeInterpreter = new OptionKey<>(false);
 
