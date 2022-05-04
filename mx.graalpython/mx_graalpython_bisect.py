05c25d3bcf258a04a0b0cba4b3cc4d81f57dfcb1
Cosmin Basca <cosmin.basca@oracle.com>
2022-05-04 15:20:22 +0200

    mx_graalpython_bisect.py: add WORKS criterion to bisect job

--- mx.graalpython/mx_graalpython_bisect.py
+++ mx.graalpython/mx_graalpython_bisect.py
@@ -238,7 +238,12 @@ def _bisect_benchmark(argv, bisect_id, email_to):
         output = mx.OutputCapture()
         retcode = mx.run(shlex.split(args.benchmark_command), out=mx.TeeOutputCapture(output), nonZeroIsFatal=False)
         if retcode:
-            raise RuntimeError("Failed to execute benchmark for {}".format(commit))
+            if args.benchmark_criterion == 'WORKS':
+                return sys.maxsize
+            else:
+                raise RuntimeError("Failed to execute benchmark for {}".format(commit))
+        elif args.benchmark_criterion == 'WORKS':
+            return 0
         match = re.search(r'{}.*duration: ([\d.]+)'.format(re.escape(args.benchmark_criterion)), output.data)
         if not match:
             raise RuntimeError("Failed to get result from the benchmark")
