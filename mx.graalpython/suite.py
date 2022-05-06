80680afee76c95c62f2f7d1741ce9a40da3f347e
Taha Alamine Boukhalf <taha.alamine.boukhalf@oracle.com>
2022-05-06 15:27:21 +0000

    new Dev Cycle 21.3.2.1

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,8 +9,8 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "21.3.2",
-    "release": True,
+    "version": "21.3.2.1",
+    "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
 
