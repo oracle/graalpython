4b64525d3a7ba452cdb80871fb21dc1a1052a83f
Florian Angerer <florian.angerer@oracle.com>
2022-06-09 18:29:26 +0200

    Add context.h and some empty impls

--- graalpython/com.oracle.graal.python.cext/include/context.h
+++ graalpython/com.oracle.graal.python.cext/include/context.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2022, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2017 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
 #ifndef Py_CONTEXT_H
 #define Py_CONTEXT_H
 #ifdef __cplusplus
