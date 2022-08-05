c8a37be2d8d4d084a7362e5baaeb72f9d20ab11b
Florian Angerer <florian.angerer@oracle.com>
2022-08-05 17:03:23 +0200

    Add C global variable PyOS_InputHook

--- graalpython/com.oracle.graal.python.cext/src/pythonrun.c
+++ graalpython/com.oracle.graal.python.cext/src/pythonrun.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -39,6 +39,8 @@
  * SOFTWARE.
  */
 #include "capi.h"
+
+int (*PyOS_InputHook)(void) = NULL;
 
 UPCALL_ID(PyRun_String);
 PyObject* PyRun_StringFlags(const char* source, int type, PyObject* globals, PyObject* locals, PyCompilerFlags* ignored) {
