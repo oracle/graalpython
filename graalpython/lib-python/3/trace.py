ef6047ccd68c3145f8aac83aa233f988e21397f2
Michael Simacek <michael.simacek@oracle.com>
2022-08-23 13:54:09 +0200

    Disable part that relies on gc.referrers

--- graalpython/lib-python/3/trace.py
+++ graalpython/lib-python/3/trace.py
@@ -490,32 +490,33 @@ class Trace:
 
         funcname = code.co_name
         clsname = None
-        if code in self._caller_cache:
-            if self._caller_cache[code] is not None:
-                clsname = self._caller_cache[code]
-        else:
-            self._caller_cache[code] = None
-            ## use of gc.get_referrers() was suggested by Michael Hudson
-            # all functions which refer to this code object
-            funcs = [f for f in gc.get_referrers(code)
-                         if inspect.isfunction(f)]
-            # require len(func) == 1 to avoid ambiguity caused by calls to
-            # new.function(): "In the face of ambiguity, refuse the
-            # temptation to guess."
-            if len(funcs) == 1:
-                dicts = [d for d in gc.get_referrers(funcs[0])
-                             if isinstance(d, dict)]
-                if len(dicts) == 1:
-                    classes = [c for c in gc.get_referrers(dicts[0])
-                                   if hasattr(c, "__bases__")]
-                    if len(classes) == 1:
-                        # ditto for new.classobj()
-                        clsname = classes[0].__name__
-                        # cache the result - assumption is that new.* is
-                        # not called later to disturb this relationship
-                        # _caller_cache could be flushed if functions in
-                        # the new module get called.
-                        self._caller_cache[code] = clsname
+        # GraalVM change: we don't support gc.get_referrers()
+        # if code in self._caller_cache:
+        #     if self._caller_cache[code] is not None:
+        #         clsname = self._caller_cache[code]
+        # else:
+        #     self._caller_cache[code] = None
+        #     ## use of gc.get_referrers() was suggested by Michael Hudson
+        #     # all functions which refer to this code object
+        #     funcs = [f for f in gc.get_referrers(code)
+        #                  if inspect.isfunction(f)]
+        #     # require len(func) == 1 to avoid ambiguity caused by calls to
+        #     # new.function(): "In the face of ambiguity, refuse the
+        #     # temptation to guess."
+        #     if len(funcs) == 1:
+        #         dicts = [d for d in gc.get_referrers(funcs[0])
+        #                      if isinstance(d, dict)]
+        #         if len(dicts) == 1:
+        #             classes = [c for c in gc.get_referrers(dicts[0])
+        #                            if hasattr(c, "__bases__")]
+        #             if len(classes) == 1:
+        #                 # ditto for new.classobj()
+        #                 clsname = classes[0].__name__
+        #                 # cache the result - assumption is that new.* is
+        #                 # not called later to disturb this relationship
+        #                 # _caller_cache could be flushed if functions in
+        #                 # the new module get called.
+        #                 self._caller_cache[code] = clsname
         if clsname is not None:
             funcname = "%s.%s" % (clsname, funcname)
 
