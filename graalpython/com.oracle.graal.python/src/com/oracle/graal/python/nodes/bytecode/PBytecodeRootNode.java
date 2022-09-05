ad3561f16243108c594bad3ca0fa6b7437818b43
Adam Hrbac <adam.hrbac@oracle.com>
2022-09-05 14:30:11 +0200

    use TruffleSafepoint.poll again

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/bytecode/PBytecodeRootNode.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/nodes/bytecode/PBytecodeRootNode.java
@@ -203,6 +203,7 @@ import com.oracle.truffle.api.HostCompilerDirectives.BytecodeInterpreterSwitch;
 import com.oracle.truffle.api.HostCompilerDirectives.InliningCutoff;
 import com.oracle.truffle.api.Truffle;
 import com.oracle.truffle.api.TruffleLanguage;
+import com.oracle.truffle.api.TruffleSafepoint;
 import com.oracle.truffle.api.exception.AbstractTruffleException;
 import com.oracle.truffle.api.frame.Frame;
 import com.oracle.truffle.api.frame.FrameDescriptor;
@@ -1882,7 +1883,7 @@ public final class PBytecodeRootNode extends PRootNode implements BytecodeOSRNod
                                 }
                             }
                         }
-                        PythonContext.triggerAsyncActions(this);
+                        TruffleSafepoint.poll(this);
                         oparg = 0;
                         continue;
                     }
