9c8ab2d5ccaa78d487fd86dcb16c826fb0b0dbc5
Gilles Duboscq <gilles.m.duboscq@oracle.com>
2022-09-19 12:17:46 +0200

    Next dev cycle

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,7 +9,7 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "22.3.0",
+    "version": "23.0.0",
     "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
