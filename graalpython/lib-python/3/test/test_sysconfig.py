c562ad3318b4a3fc708ea329e443321bc4d59faa
Tim Felgentreff <tim.felgentreff@oracle.com>
2022-12-02 13:03:45 +0100

    remove one of our test_sysconfig.py patches

--- graalpython/lib-python/3/test/test_sysconfig.py
+++ graalpython/lib-python/3/test/test_sysconfig.py
@@ -263,7 +263,7 @@ class TestSysConfig(unittest.TestCase):
         self.assertTrue(os.path.isfile(config_h), config_h)
 
     def test_get_scheme_names(self):
-        wanted = ['nt', 'posix_home', 'posix_prefix', 'graalpy']
+        wanted = ['nt', 'posix_home', 'posix_prefix']
         if HAS_USER_BASE:
             wanted.extend(['nt_user', 'osx_framework_user', 'posix_user'])
         self.assertEqual(get_scheme_names(), tuple(sorted(wanted)))
