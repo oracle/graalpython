a77c8372bae245dc50d859dbca7ecfaf364584e0
Florian Angerer <florian.angerer@oracle.com>
2022-12-27 15:31:30 +0100

    Fix invalid ptr-to-int conversion in c-magic-iter

--- graalpython/com.oracle.graal.python.benchmarks/python/micro/c-magic-iter.py
+++ graalpython/com.oracle.graal.python.benchmarks/python/micro/c-magic-iter.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019, 2021, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -186,7 +186,7 @@ int ci_init(PyObject* self, PyObject* args, PyObject* kwds) {
     Py_ssize_t n = 0;
 
     if (!PyArg_ParseTupleAndKeywords(args, kwds, "n", kwlist, &n)) {
-        return NULL;
+        return 1;
     }
     NativeCustomIterableObject* tself = (NativeCustomIterableObject*)self;
     tself->scale = n + 1;
