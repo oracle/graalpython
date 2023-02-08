7a42c291694b257c1b9e3e1c5256e082ed8e03de
Lukas Stadler <lukas.stadler@oracle.com>
2023-02-08 10:17:18 +0100

    more copyright years

--- graalpython/com.oracle.graal.python.cext/include/hpy/cpython/hpyfunc_trampolines.h
+++ graalpython/com.oracle.graal.python.cext/include/hpy/cpython/hpyfunc_trampolines.h
@@ -1,6 +1,6 @@
 /* MIT License
  *
- * Copyright (c) 2020, 2022, Oracle and/or its affiliates.
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates.
  * Copyright (c) 2019 pyhandle
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
