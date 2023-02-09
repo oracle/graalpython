2ded4001851992725686d87fa4b11b260eae6a83
Florian Angerer <florian.angerer@oracle.com>
2023-02-09 20:31:56 +0100

    Do not wrap capsule name in CStringWrapper for NFI

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/cext/PythonCextCapsuleBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/cext/PythonCextCapsuleBuiltins.java
@@ -186,9 +186,6 @@ public final class PythonCextCapsuleBuiltins {
             if (o.getName() == null) {
                 return getNULL();
             }
-            if (o.getName() instanceof TruffleString) {
-                return new CArrayWrappers.CStringWrapper((TruffleString) o.getName());
-            }
             return o.getName();
         }
 
