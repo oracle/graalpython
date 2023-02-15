26726601cf9e665222cbabbca36d6e0f61397b18
Lukas Stadler <lukas.stadler@oracle.com>
2023-02-15 17:18:54 +0100

    avoid testing for equality of integers in test_method.py

--- graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_method.py
+++ graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_method.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021, 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2021, 2023, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -186,7 +186,7 @@ class TestPyMethod(CPyExtTestCase):
     test_PyMethod_New = CPyExtFunction(
         lambda args: types.MethodType(*args),
         lambda: (
-            (list.append, 6),
+            (list.append, str),
         ),
         resultspec="O",
         argspec='OO',
