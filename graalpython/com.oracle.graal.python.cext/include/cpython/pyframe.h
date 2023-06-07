e8b7fd60562eb589de66a9ab15bc8c3da3cb2a94
Michael Simacek <michael.simacek@oracle.com>
2023-06-07 15:41:35 +0200

    Backport pyframe.h header from python 3.11

---
+++ graalpython/com.oracle.graal.python.cext/include/cpython/pyframe.h
@@ -0,0 +1,12 @@
+#ifndef Py_CPYTHON_PYFRAME_H
+#  error "this header file must not be included directly"
+#endif
+
+PyAPI_FUNC(PyFrameObject *) PyFrame_GetBack(PyFrameObject *frame);
+PyAPI_FUNC(PyObject *) PyFrame_GetLocals(PyFrameObject *frame);
+
+PyAPI_FUNC(PyObject *) PyFrame_GetGlobals(PyFrameObject *frame);
+PyAPI_FUNC(PyObject *) PyFrame_GetBuiltins(PyFrameObject *frame);
+
+PyAPI_FUNC(PyObject *) PyFrame_GetGenerator(PyFrameObject *frame);
+PyAPI_FUNC(int) PyFrame_GetLasti(PyFrameObject *frame);
