02c473adab96ebd18d2103fd61ca06cd1d1bbc02
Tim Felgentreff <tim.felgentreff@oracle.com>
2023-06-19 11:28:12 +0200

    Add EE python component to standalone for oracle graalvm

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -2016,6 +2016,7 @@ mx_sdk.register_graalvm_component(mx_sdk.GraalVmLanguage(
     standalone_dependencies_enterprise={**standalone_dependencies_common, **{
         'LLVM Runtime Enterprise': ('lib/sulong', []),
         'LLVM Runtime Native Enterprise': ('lib/sulong', []),
+        'GraalVM Python EE': ('', []),
         'GraalVM Python license files EE': ('', []),
         'GraalVM enterprise license files': ('', ['LICENSE.txt', 'GRAALVM-README.md']),
     }},
