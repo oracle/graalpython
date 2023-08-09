dfd4f820354d27589570b757640e28b713ba7928
stepan <stepan.sindelar@oracle.com>
2023-08-09 23:21:02 +0200

    Bump required mx version in suite.py

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -5,7 +5,7 @@ suite = {
     #  METADATA
     #
     # --------------------------------------------------------------------------------------------------------------
-    "mxversion": "6.37.0",
+    "mxversion": "6.40.1",
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
