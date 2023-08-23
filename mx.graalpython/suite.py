cf46ad16980b613f200c7d352009bc9b13249d96
stepan <stepan.sindelar@oracle.com>
2023-08-23 18:01:36 +0200

    Use fully optional Truffle resources

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -393,7 +393,6 @@ suite = {
             "dependencies": [
                 "com.oracle.graal.python.annotations",
                 "com.oracle.graal.python.pegparser",
-                "com.oracle.graal.python.resources",
                 "truffle:TRUFFLE_API",
                 "truffle:TRUFFLE_NFI",
                 "tools:TRUFFLE_PROFILER",
@@ -915,9 +914,6 @@ suite = {
             "platformDependent": False,
             "moduleInfo": {
                 "name": "org.graalvm.py.resources",
-                "exports": [
-                    "com.oracle.graal.python.resources to org.graalvm.py",
-                ],
             },
             # "useModulePath": True,
             "dependencies": [
@@ -957,9 +953,6 @@ suite = {
                 "uses": [
                     "com.oracle.graal.python.builtins.PythonBuiltins",
                 ],
-                "requires": [
-                    "static org.graalvm.py.resources",
-                ],
             },
             # "useModulePath": True,
             "dependencies": [
@@ -975,7 +968,6 @@ suite = {
                 "sulong:SULONG_API",
                 "sulong:SULONG_NATIVE",  # this is actually just a runtime dependency
                 "truffle:TRUFFLE_ICU4J",
-                "GRAALPYTHON_RESOURCES", # overridden below to make this an optional dependency
             ],
             "requires": [
                 "java.base",
