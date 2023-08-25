79d43a92abc530c87f0aef1d6f15ee720745c8cc
Mohaned Qunaibit <mohaned.qunaibit@oracle.com>
2023-08-25 18:03:53 +0300

    Use PyHeapTypeObject instead of PyObject

--- graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_object.py
+++ graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_object.py
@@ -582,7 +582,7 @@ class TestObject(object):
                 PyType_Slot slots[] = {
                     { 0 }
                 };
-                PyType_Spec spec = { "DynamicType", sizeof(PyObject), 0, Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, slots };
+                PyType_Spec spec = { "DynamicType", sizeof(PyHeapTypeObject), 0, Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE, slots };
                 PyObject* result = PyType_FromSpecWithBases(&spec, bases);
                 return result;
             }
