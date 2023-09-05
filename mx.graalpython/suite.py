b3b0e9b9c58699578fcd2e3d90f6599f92c86bda
stepan <stepan.sindelar@oracle.com>
2023-09-05 11:58:10 +0200

    TMP: update Truffle import to GR-48436

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -45,7 +45,7 @@ suite = {
             },
             {
                 "name": "sdk",
-                "version": "1eee8373e83d89c89909bc463f6ce57e8d80adfc",
+                "version": "13c2e4059ab426d6ed3e09d2e55c2d06ac6450e2",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
@@ -53,7 +53,7 @@ suite = {
             },
             {
                 "name": "tools",
-                "version": "1eee8373e83d89c89909bc463f6ce57e8d80adfc",
+                "version": "13c2e4059ab426d6ed3e09d2e55c2d06ac6450e2",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
@@ -61,7 +61,7 @@ suite = {
             },
             {
                 "name": "sulong",
-                "version": "1eee8373e83d89c89909bc463f6ce57e8d80adfc",
+                "version": "13c2e4059ab426d6ed3e09d2e55c2d06ac6450e2",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
@@ -69,7 +69,7 @@ suite = {
             },
             {
                 "name": "regex",
-                "version": "1eee8373e83d89c89909bc463f6ce57e8d80adfc",
+                "version": "13c2e4059ab426d6ed3e09d2e55c2d06ac6450e2",
                 "subdir": True,
                 "urls": [
                     {"url": "https://github.com/oracle/graal", "kind": "git"},
