58bdd7de09d53be05d92a9f12a23af3d6c38c425
stepan <stepan.sindelar@oracle.com>
2023-09-05 10:53:59 +0200

    Temp: add more logging to test_standalone.py
    (cherry picked from commit d14cdcbfe077a4daa57899baebcd8e6bcd58aebf)

--- graalpython/lib-graalpython/modules/standalone/app/graalpy.sh
+++ graalpython/lib-graalpython/modules/standalone/app/graalpy.sh
@@ -17,4 +17,5 @@ for var in "$@"; do
 done
 
 export GRAAL_PYTHON_ARGS=$args
+echo graalpy.sh is going to execute: mvn -f "${location}/pom.xml" exec:exec -Dexec.executable=java -Dexec.args="--module-path %classpath '-Dorg.graalvm.launcher.executablename=$0' --module org.graalvm.py.launcher/com.oracle.graal.python.shell.GraalPythonMain"
 mvn -f "${location}/pom.xml" exec:exec -Dexec.executable=java -Dexec.args="--module-path %classpath '-Dorg.graalvm.launcher.executablename=$0' --module org.graalvm.py.launcher/com.oracle.graal.python.shell.GraalPythonMain"
