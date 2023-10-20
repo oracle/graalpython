249facdaa87860239cb454df7732d6e810224f9e
Anas El korchi <anas.el.korchi@oracle.com>
2023-10-20 14:56:37 +0100

    [GS-5266] New dev cycle GraalVM 23.0.3

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -9,8 +9,8 @@ suite = {
     "name": "graalpython",
     "versionConflictResolution": "latest",
 
-    "version": "23.0.2",
-    "release": True,
+    "version": "23.0.3",
+    "release": False,
     "groupId": "org.graalvm.graalpython",
     "url": "http://www.graalvm.org/",
 
