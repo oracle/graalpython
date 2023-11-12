100b6f258f52173fb536612acd0651894fb1f0ca
Mohaned Qunaibit <mohaned.qunaibit@oracle.com>
2023-11-12 21:24:27 +0300

    enable svm's `UnlockExperimentalVMOptions` option

--- graalpython/lib-graalpython/modules/standalone/__main__.py
+++ graalpython/lib-graalpython/modules/standalone/__main__.py
@@ -427,6 +427,7 @@ def build_binary(target_dir, ni, jc, modules_path, launcher_file, parsed_args):
         cmd += [
             "--no-fallback",
             "-H:-CopyLanguageResources",
+            "-H:+UnlockExperimentalVMOptions",
             f"-H:ResourceConfigurationFiles={target_dir}/{NATIVE_IMAGE_RESOURCES_FILE}",
             "-o",
             output,
