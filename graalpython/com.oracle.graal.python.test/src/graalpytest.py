eeb573c90eac61ec61a105a692b8f8346ea7a32d
stepan <stepan.sindelar@oracle.com>
2024-01-12 09:19:05 +0100

    Force serial run for test_multiprocessing_graalpy, reset start method after the test

--- graalpython/com.oracle.graal.python.test/src/graalpytest.py
+++ graalpython/com.oracle.graal.python.test/src/graalpytest.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -66,6 +66,7 @@ SERIAL_TESTS = [
     'test_imaplib',
     "test_ftplib",
     'test_multiprocessing_spawn',
+    'test_multiprocessing_graalpy',
     # trying to avoid transient issues there, not sure about the reason
     'test_unittest',
     'test_glob',
