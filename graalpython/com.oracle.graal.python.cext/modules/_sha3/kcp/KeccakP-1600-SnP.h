11e597bb135f67c4f417d69063ba7c9ef22cf45a
Mohaned Qunaibit <mohaned.qunaibit@oracle.com>
2024-01-18 04:46:46 +0300

    Update _sha3 license to PSF format

--- graalpython/com.oracle.graal.python.cext/modules/_sha3/kcp/KeccakP-1600-SnP.h
+++ graalpython/com.oracle.graal.python.cext/modules/_sha3/kcp/KeccakP-1600-SnP.h
@@ -1,3 +1,8 @@
+/* Copyright (c) 2024, Oracle and/or its affiliates.
+ * Copyright (C) 1996-2024 Python Software Foundation
+ *
+ * Licensed under the PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+ */
 #if KeccakOpt == 64
   #include "KeccakP-1600-SnP-opt64.h"
 #elif KeccakOpt == 32
