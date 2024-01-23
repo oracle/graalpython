96df1351732f6ef1908cb480896ff92100cf523d
stepan <stepan.sindelar@oracle.com>
2024-01-23 11:54:04 +0100

    Fix super.__get__ with already bound or non-binding super

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/superobject/SuperBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/superobject/SuperBuiltins.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -391,7 +391,7 @@ public final class SuperBuiltins extends PythonBuiltins {
                         @Cached GetObjectNode getObject,
                         @Cached DoGetNode doGetNode) {
             if (objIsNoneProfile.profile(inliningTarget, PGuards.isNone(obj)) || getObject.execute(inliningTarget, self) != null) {
-                return this;
+                return self;
             }
             return doGetNode.execute(inliningTarget, self, obj);
         }
