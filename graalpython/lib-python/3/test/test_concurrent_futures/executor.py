664c7fbe08d3d2db01d675a2c877e125f1802427
Michael Simacek <michael.simacek@oracle.com>
2024-02-02 12:39:13 +0100

    Re-add change to test_concurent_futures

--- graalpython/lib-python/3/test/test_concurrent_futures/executor.py
+++ graalpython/lib-python/3/test/test_concurrent_futures/executor.py
@@ -55,6 +55,8 @@ class ExecutorTest:
 
     def test_map_timeout(self):
         results = []
+        # GraalPy change: submit some dummy work first, so the next map call doesn't time out in the worker start up
+        list(self.executor.map(time.sleep, [0]))
         try:
             for i in self.executor.map(time.sleep,
                                        [0, 0, 6],
