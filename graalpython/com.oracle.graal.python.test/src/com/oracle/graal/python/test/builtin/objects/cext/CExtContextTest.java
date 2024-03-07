e1f4afc8c1c2f5f1a2d51de88c7bd9a46f17f37c
Florian Angerer <florian.angerer@oracle.com>
2024-03-07 22:11:49 +0100

    Fixup CExtContextTest

--- graalpython/com.oracle.graal.python.test/src/com/oracle/graal/python/test/builtin/objects/cext/CExtContextTest.java
+++ graalpython/com.oracle.graal.python.test/src/com/oracle/graal/python/test/builtin/objects/cext/CExtContextTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022, 2023, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2022, 2024, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -82,11 +82,6 @@ public class CExtContextTest {
     private static class TestCExtContext extends CExtContext {
         public TestCExtContext(PythonContext context, Object llvmLibrary) {
             super(context, llvmLibrary, false);
-        }
-
-        @Override
-        protected Store initializeSymbolCache() {
-            throw new RuntimeException("should not reach here");
         }
 
         public static TruffleString getBN(TruffleString s) {
