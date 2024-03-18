c0b632b75c97a13651e45a55ea722f77ae4bd0c0
Florian Angerer <florian.angerer@oracle.com>
2024-03-18 11:59:36 +0100

    Register fields PyThreadState.curexc_*

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/structs/CFields.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/structs/CFields.java
@@ -333,6 +333,9 @@ public enum CFields {
     PyMemberDef__flags(Int),
     PyMemberDef__doc(ConstCharPtr),
 
+    PyThreadState__curexc_type(PyObject),
+    PyThreadState__curexc_value(PyObject),
+    PyThreadState__curexc_traceback(PyObject),
     PyThreadState__dict(PyObject),
     PyThreadState__small_ints(PyObjectPtr),
 
