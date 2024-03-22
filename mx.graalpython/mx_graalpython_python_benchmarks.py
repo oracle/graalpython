f9f6b5f282ca2f6ee6eb5af5288b85bb07024803
Michael Simacek <michael.simacek@oracle.com>
2024-03-22 16:53:50 +0100

    Return the actual return code from benchmarks

--- mx.graalpython/mx_graalpython_python_benchmarks.py
+++ mx.graalpython/mx_graalpython_python_benchmarks.py
@@ -571,7 +571,7 @@ class PyPerformanceSuite(PySuite):
         mx.log(f"Return code of benchmark harness: {retcode}")
         shutil.copy(join(workdir, json_file), join(SUITE.dir, "raw_results.json"))
         shutil.copy(join(workdir, json_file_memory), join(SUITE.dir, "raw_results_memory.json"))
-        return 0, ",".join([join(workdir, json_file), join(workdir, json_file_memory)])
+        return retcode, ",".join([join(workdir, json_file), join(workdir, json_file_memory)])
 
 
 class PyPySuite(PySuite):
@@ -652,7 +652,7 @@ class PyPySuite(PySuite):
         )
         shutil.copy(join(workdir, json_file), join(SUITE.dir, "raw_results.json"))
         mx.log(f"Return code of benchmark harness: {retcode}")
-        return 0, join(workdir, json_file)
+        return retcode, join(workdir, json_file)
 
 
 class NumPySuite(PySuite):
@@ -766,7 +766,7 @@ class NumPySuite(PySuite):
         if json_file:
             json_file = json_file[0]
             shutil.copy(json_file, join(SUITE.dir, "raw_results.json"))
-            return 0, json_file
+            return retcode, json_file
         else:
             return -1, ""
 
@@ -906,7 +906,7 @@ class PandasSuite(PySuite):
         if json_file:
             json_file = json_file[0]
             shutil.copy(json_file, join(SUITE.dir, "raw_results.json"))
-            return 0, json_file
+            return retcode, json_file
         else:
             return -1, ""
 
