60c8b6560cecfa0bbef654a199463972b3f2c855
stepan <stepan.sindelar@oracle.com>
2024-04-09 09:53:24 +0200

    Remove NoneType.__get__ hack

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/NoneBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/NoneBuiltins.java
@@ -40,7 +40,6 @@
  */
 package com.oracle.graal.python.builtins.objects;
 
-import static com.oracle.graal.python.builtins.PythonBuiltinClassType.AttributeError;
 import static com.oracle.graal.python.nodes.SpecialMethodNames.J___REPR__;
 import static com.oracle.graal.python.nodes.StringLiterals.T_NONE;
 
@@ -52,30 +51,14 @@ import com.oracle.graal.python.builtins.Builtin;
 import com.oracle.graal.python.builtins.CoreFunctions;
 import com.oracle.graal.python.builtins.PythonBuiltinClassType;
 import com.oracle.graal.python.builtins.PythonBuiltins;
-import com.oracle.graal.python.builtins.objects.function.PBuiltinFunction;
-import com.oracle.graal.python.builtins.objects.getsetdescriptor.GetSetDescriptor;
-import com.oracle.graal.python.builtins.objects.object.ObjectBuiltins;
 import com.oracle.graal.python.builtins.objects.type.TpSlots;
-import com.oracle.graal.python.builtins.objects.type.slots.TpSlotGetAttr.GetAttrBuiltinNode;
 import com.oracle.graal.python.builtins.objects.type.slots.TpSlotInquiry.NbBoolBuiltinNode;
-import com.oracle.graal.python.nodes.ErrorMessages;
-import com.oracle.graal.python.nodes.PRaiseNode;
-import com.oracle.graal.python.nodes.attributes.LookupAttributeInMRONode;
-import com.oracle.graal.python.nodes.call.special.CallUnaryMethodNode;
 import com.oracle.graal.python.nodes.function.PythonBuiltinBaseNode;
 import com.oracle.graal.python.nodes.function.builtins.PythonUnaryBuiltinNode;
-import com.oracle.graal.python.nodes.util.CannotCastException;
-import com.oracle.graal.python.nodes.util.CastToTruffleStringNode;
-import com.oracle.graal.python.runtime.object.PythonObjectFactory;
-import com.oracle.truffle.api.dsl.Bind;
-import com.oracle.truffle.api.dsl.Cached;
 import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.GenerateUncached;
 import com.oracle.truffle.api.dsl.NodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
-import com.oracle.truffle.api.frame.VirtualFrame;
-import com.oracle.truffle.api.nodes.Node;
-import com.oracle.truffle.api.strings.TruffleString;
 
 @CoreFunctions(extendClasses = PythonBuiltinClassType.PNone)
 public final class NoneBuiltins extends PythonBuiltins {
@@ -104,57 +87,6 @@ public final class NoneBuiltins extends PythonBuiltins {
         static Object doNone(PNone none) {
             assert none == PNone.NONE;
             return T_NONE;
-        }
-    }
-
-    /**
-     * XXX CPython's None doesn't declare its own __getattribute__, it inherits the object one. We
-     * currently have to have one because of peculiarities in descriptor protocol. CPython has a
-     * slot wrapper for __get__ that converts None to NULL, which is then treated as a descriptor
-     * get on a type. object.__getattribute__ calls tp_descr_get which doesn't go through the
-     * wrapper, so the None stays as None. IOW you can't express descriptor gets on None in terms of
-     * __get__, i.e. None.__class__ is not the same as object.__dict__['__class__'].__get__(None,
-     * type(None)). Since we don't distinguish between method and slot calls, this is the current
-     * workaround that bypasses the descriptor __get__ handling and invokes the get function
-     * directly.
-     */
-    @Slot(value = SlotKind.tp_get_attro, isComplex = true)
-    @GenerateNodeFactory
-    public abstract static class GetAttributeNode extends GetAttrBuiltinNode {
-
-        @Specialization
-        static Object doIt(VirtualFrame frame, Object object, Object keyObj,
-                        @Bind("this") Node inliningTarget,
-                        @Cached LookupAttributeInMRONode.Dynamic lookup,
-                        @Cached CallUnaryMethodNode callGet,
-                        @Cached CastToTruffleStringNode castKeyToStringNode,
-                        @Cached PythonObjectFactory.Lazy factory,
-                        @Cached ObjectBuiltins.GetAttributeNode getAttributeNode,
-                        @Cached PRaiseNode.Lazy raiseNode) {
-            assert object == PNone.NONE;
-            TruffleString key;
-            try {
-                key = castKeyToStringNode.execute(inliningTarget, keyObj);
-            } catch (CannotCastException e) {
-                throw raiseNode.get(inliningTarget).raise(PythonBuiltinClassType.TypeError, ErrorMessages.ATTR_NAME_MUST_BE_STRING, keyObj);
-            }
-
-            Object descr = lookup.execute(PythonBuiltinClassType.PNone, key);
-            if (descr == PNone.NO_VALUE) {
-                throw raiseNode.get(inliningTarget).raise(AttributeError, ErrorMessages.OBJ_P_HAS_NO_ATTR_S, PNone.NONE, key);
-            }
-            if (descr instanceof GetSetDescriptor getSetDescriptor) {
-                // Bypass getset_descriptor.__get__
-                assert getSetDescriptor.getGet() != null;
-                return callGet.executeObject(frame, getSetDescriptor.getGet(), PNone.NONE);
-            }
-            if (descr instanceof PBuiltinFunction function) {
-                // Bypass method_descriptor.__get__
-                assert !function.needsDeclaringType();
-                return factory.get(inliningTarget).createBuiltinMethod(PNone.NONE, function);
-            }
-            // Delegate classmethods, staticmethods etc. to object.__getattribute__
-            return getAttributeNode.execute(frame, PNone.NONE, key);
         }
     }
 }
