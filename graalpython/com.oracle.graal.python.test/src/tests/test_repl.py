695a13b9636bb182a7ff68fc0b2bca8e801c6aba
Michael Simacek <michael.simacek@oracle.com>
2024-07-23 10:39:02 +0200

    Skip test_repl on windows and managed

--- graalpython/com.oracle.graal.python.test/src/tests/test_repl.py
+++ graalpython/com.oracle.graal.python.test/src/tests/test_repl.py
@@ -36,158 +36,161 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import os
-import re
-import select
-import subprocess
+
 import sys
-import tempfile
-import termios
-from textwrap import dedent
+
+if sys.platform != 'win32' and (sys.implementation.name != 'graalpy' or not __graalpython__.is_managed_launcher()):
+    import os
+    import re
+    import select
+    import subprocess
+    import tempfile
+    import termios
+    from textwrap import dedent
 
 
-def validate_repl(stdin, python_args=(), ignore_preamble=True):
-    env = os.environ.copy()
-    env['TERM'] = 'ansi'
-    env['PYTHONIOENCODING'] = 'utf-8'
-    pty_parent, pty_child = os.openpty()
-    try:
-        termios.tcsetwinsize(pty_parent, (60, 80))
-        proc = subprocess.Popen(
-            [sys.executable, '-I', *python_args],
-            env=env,
-            stdin=pty_child,
-            stdout=pty_child,
-            stderr=pty_child,
-        )
-        out = ''
-        input_and_output = []
-        expected_preamble = ''
-        in_matches = list(re.finditer(r'^(>>>|\.\.\.) (.*)', stdin, flags=re.MULTILINE))
-        for i, match in enumerate(in_matches):
-            if i == 0:
-                expected_preamble = stdin[:match.start() - 1] if match.start() else ''
-            input_and_output.append((
-                match.group(1),
-                match.group(2),
-                stdin[match.end():in_matches[i + 1].start() - 1 if i + 1 < len(in_matches) else -1],
-            ))
-        index = -1
-        whole_out = ''
-        while True:
-            rlist, _, _ = select.select([pty_parent], [], [], 30)
-            assert pty_parent in rlist, f"Timed out waiting for REPL output. Output: {whole_out}{out}"
-            out += os.read(pty_parent, 1024).decode('utf-8')
-            out = out.replace('\r\n', '\n')
-            out = re.sub(r'\x1b\[(?:\?2004[hl]|\d+[A-G])', '', out)
-            if out == '>>> ' or out.endswith(('\n>>> ', '\n... ')):
-                prompt = out[:3]
-                actual = out[:-5]
-                if index >= 0:
-                    expected_prompt, current_in, expected_out = input_and_output[index]
-                    assert prompt == expected_prompt
-                    expected = f'{expected_prompt} {current_in}{expected_out}'
-                else:
-                    expected = expected_preamble
-                if index >= 0 or not ignore_preamble:
-                    assert actual == expected, f'Actual:\n{actual!r}\nExpected:\n{expected!r}'
-                index += 1
-                whole_out += out[:-4]
-                out = out[-4:]
-                if index >= len(input_and_output):
-                    os.write(pty_parent, b'\x04')  # CTRL-D
-                    proc.wait(timeout=30)
-                    out = os.read(pty_parent, 1024).decode('utf-8')
-                    out = re.sub(r'\x1b\[\?2004[hl]', '', out)
-                    assert not out.strip(), f"Garbage after EOF:\n{out!r}"
-                    return
-                else:
-                    _, next_in, _ = input_and_output[index]
-                    os.write(pty_parent, next_in.encode('utf-8') + b'\r')
-    finally:
-        os.close(pty_child)
-        os.close(pty_parent)
+    def validate_repl(stdin, python_args=(), ignore_preamble=True):
+        env = os.environ.copy()
+        env['TERM'] = 'ansi'
+        env['PYTHONIOENCODING'] = 'utf-8'
+        pty_parent, pty_child = os.openpty()
+        try:
+            termios.tcsetwinsize(pty_parent, (60, 80))
+            proc = subprocess.Popen(
+                [sys.executable, '-I', *python_args],
+                env=env,
+                stdin=pty_child,
+                stdout=pty_child,
+                stderr=pty_child,
+            )
+            out = ''
+            input_and_output = []
+            expected_preamble = ''
+            in_matches = list(re.finditer(r'^(>>>|\.\.\.) (.*)', stdin, flags=re.MULTILINE))
+            for i, match in enumerate(in_matches):
+                if i == 0:
+                    expected_preamble = stdin[:match.start() - 1] if match.start() else ''
+                input_and_output.append((
+                    match.group(1),
+                    match.group(2),
+                    stdin[match.end():in_matches[i + 1].start() - 1 if i + 1 < len(in_matches) else -1],
+                ))
+            index = -1
+            whole_out = ''
+            while True:
+                rlist, _, _ = select.select([pty_parent], [], [], 30)
+                assert pty_parent in rlist, f"Timed out waiting for REPL output. Output: {whole_out}{out}"
+                out += os.read(pty_parent, 1024).decode('utf-8')
+                out = out.replace('\r\n', '\n')
+                out = re.sub(r'\x1b\[(?:\?2004[hl]|\d+[A-G])', '', out)
+                if out == '>>> ' or out.endswith(('\n>>> ', '\n... ')):
+                    prompt = out[:3]
+                    actual = out[:-5]
+                    if index >= 0:
+                        expected_prompt, current_in, expected_out = input_and_output[index]
+                        assert prompt == expected_prompt
+                        expected = f'{expected_prompt} {current_in}{expected_out}'
+                    else:
+                        expected = expected_preamble
+                    if index >= 0 or not ignore_preamble:
+                        assert actual == expected, f'Actual:\n{actual!r}\nExpected:\n{expected!r}'
+                    index += 1
+                    whole_out += out[:-4]
+                    out = out[-4:]
+                    if index >= len(input_and_output):
+                        os.write(pty_parent, b'\x04')  # CTRL-D
+                        proc.wait(timeout=30)
+                        out = os.read(pty_parent, 1024).decode('utf-8')
+                        out = re.sub(r'\x1b\[\?2004[hl]', '', out)
+                        assert not out.strip(), f"Garbage after EOF:\n{out!r}"
+                        return
+                    else:
+                        _, next_in, _ = input_and_output[index]
+                        os.write(pty_parent, next_in.encode('utf-8') + b'\r')
+        finally:
+            os.close(pty_child)
+            os.close(pty_parent)
 
 
-def test_basic_repl():
-    validate_repl(dedent("""\
-        >>> 1023 + 1
-        1024
-        >>> None
-        >>> "hello"
-        'hello'
-        >>> _
-        'hello'
-    """))
-
-
-def test_continuation():
-    validate_repl(dedent(r'''\
-        >>> def foo():
-        ...   a = 1
-        ...   return a
-        ... 
-        >>> class Foo:
-        ...   def meth(self):
-        ...     return 1
-        ... 
-        >>> from functools import wraps
-        >>> @wraps
-        ... def foo(fn):
-        ...   return fn
-        ... 
-        >>> from contextlib import contextmanager
-        >>> @contextmanager
-        ... class Foo:
-        ...   pass
-        ... 
-        >>> """
-        ... asdf
-        ... """
-        '\nasdf\n'
-    '''))
-
-
-def test_exceptions():
-    validate_repl(dedent("""\
-        >>> 1 / 0
-        Traceback (most recent call last):
-          File "<stdin>", line 1, in <module>
-        ZeroDivisionError: division by zero
-        >>> import sys
-        >>> sys.last_value
-        ZeroDivisionError('division by zero')
-        >>> class BrokenRepr:
-        ...   def __repr__(self):
-        ...     asdf
-        ... 
-        >>> BrokenRepr()
-        Traceback (most recent call last):
-          File "<stdin>", line 1, in <module>
-          File "<stdin>", line 3, in __repr__
-        NameError: name 'asdf' is not defined
-    """))
-
-
-def test_inspect_flag():
-    with tempfile.NamedTemporaryFile('w') as f:
-        f.write('a = 1\n')
-        f.flush()
+    def test_basic_repl():
         validate_repl(dedent("""\
-            >>> a
-            1
-        """), python_args=['-i', f.name], ignore_preamble=False)
+            >>> 1023 + 1
+            1024
+            >>> None
+            >>> "hello"
+            'hello'
+            >>> _
+            'hello'
+        """))
 
 
-def test_inspect_flag_exit():
-    with tempfile.NamedTemporaryFile('w') as f:
-        f.write('a = 1\nimport sys\nsys.exit(1)\n')
-        f.flush()
-        validate_repl(dedent(f"""\
+    def test_continuation():
+        validate_repl(dedent(r'''\
+            >>> def foo():
+            ...   a = 1
+            ...   return a
+            ... 
+            >>> class Foo:
+            ...   def meth(self):
+            ...     return 1
+            ... 
+            >>> from functools import wraps
+            >>> @wraps
+            ... def foo(fn):
+            ...   return fn
+            ... 
+            >>> from contextlib import contextmanager
+            >>> @contextmanager
+            ... class Foo:
+            ...   pass
+            ... 
+            >>> """
+            ... asdf
+            ... """
+            '\nasdf\n'
+        '''))
+
+
+    def test_exceptions():
+        validate_repl(dedent("""\
+            >>> 1 / 0
             Traceback (most recent call last):
-              File "{os.path.realpath(f.name)}", line 3, in <module>
-                sys.exit(1)
-            SystemExit: 1
-            >>> a
-            1
-        """), python_args=['-i', f.name], ignore_preamble=False)
+              File "<stdin>", line 1, in <module>
+            ZeroDivisionError: division by zero
+            >>> import sys
+            >>> sys.last_value
+            ZeroDivisionError('division by zero')
+            >>> class BrokenRepr:
+            ...   def __repr__(self):
+            ...     asdf
+            ... 
+            >>> BrokenRepr()
+            Traceback (most recent call last):
+              File "<stdin>", line 1, in <module>
+              File "<stdin>", line 3, in __repr__
+            NameError: name 'asdf' is not defined
+        """))
+
+
+    def test_inspect_flag():
+        with tempfile.NamedTemporaryFile('w') as f:
+            f.write('a = 1\n')
+            f.flush()
+            validate_repl(dedent("""\
+                >>> a
+                1
+            """), python_args=['-i', f.name], ignore_preamble=False)
+
+
+    def test_inspect_flag_exit():
+        with tempfile.NamedTemporaryFile('w') as f:
+            f.write('a = 1\nimport sys\nsys.exit(1)\n')
+            f.flush()
+            validate_repl(dedent(f"""\
+                Traceback (most recent call last):
+                  File "{os.path.realpath(f.name)}", line 3, in <module>
+                    sys.exit(1)
+                SystemExit: 1
+                >>> a
+                1
+            """), python_args=['-i', f.name], ignore_preamble=False)
