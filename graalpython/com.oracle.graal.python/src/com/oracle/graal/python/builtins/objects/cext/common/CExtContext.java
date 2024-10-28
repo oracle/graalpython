9d6c033d3bc9468c88a6b5bdf0682a263bd00654
Tim Felgentreff <tim.felgentreff@oracle.com>
2024-10-28 06:48:50 +0100

    Extend C extension warning for Windows

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/common/CExtContext.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/common/CExtContext.java
@@ -59,6 +59,7 @@ import java.util.logging.Level;
 import org.graalvm.shadowed.com.ibm.icu.impl.Punycode;
 import org.graalvm.shadowed.com.ibm.icu.text.StringPrepParseException;
 
+import com.oracle.graal.python.builtins.PythonOS;
 import com.oracle.graal.python.builtins.objects.cext.capi.CApiContext;
 import com.oracle.graal.python.builtins.objects.cext.common.CExtCommonNodes.CheckFunctionResultNode;
 import com.oracle.graal.python.builtins.objects.cext.common.LoadCExtException.ApiInitException;
@@ -309,7 +310,10 @@ public abstract class CExtContext {
                 String message = "Loading C extension module %s from '%s'. Support for the Python C API is considered experimental.";
                 if (!runViaLauncher) {
                     message += " See https://www.graalvm.org/latest/reference-manual/python/Native-Extensions/#embedding-limitations for the limitations. " +
-                                    "You can suppress this warning by setting the context option 'python.WarnExperimentalFeatures' to 'false'";
+                                    "You can suppress this warning by setting the context option 'python.WarnExperimentalFeatures' to 'false'.";
+                }
+                if (PythonOS.getPythonOS() == PythonOS.PLATFORM_WIN32) {
+                    message += " If loading binary wheels fails, please make sure you installed the latest Microsoft Visual C++ Redistributable from https://aka.ms/vs/17/release/vc_redist.x64.exe.";
                 }
                 getLogger().warning(message.formatted(spec.name, spec.path));
             }
