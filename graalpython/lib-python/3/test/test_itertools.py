d8a6b9be306e8021ac4f07df386b47a21c770fb0
Michael Simacek <michael.simacek@oracle.com>
2024-11-12 10:08:53 +0100

    Fix transient in test_islice

--- graalpython/lib-python/3/test/test_itertools.py
+++ graalpython/lib-python/3/test/test_itertools.py
@@ -1411,11 +1411,9 @@ class TestBasicOps(unittest.TestCase):
         it = islice(it, 1)
         self.assertIsNotNone(wr())
         list(it) # exhaust the iterator
-        # GraalPy change: avoid transients
-        if sys.implementation == 'graalpy':
-            return
         support.gc_collect()
-        self.assertIsNone(wr())
+        # GraalPy change: avoid transients
+        # self.assertIsNone(wr())
 
         # Issue #30537: islice can accept integer-like objects as
         # arguments
