3b0a2ee7f5bf3a5cf2d636d902d667f17eb50327
stepan <stepan.sindelar@oracle.com>
2024-11-25 16:23:24 +0100

    Ignore javac warnings temporarily until sun.misc.* API usages are migrated

--- mx.graalpython/suite.py
+++ mx.graalpython/suite.py
@@ -460,6 +460,8 @@ suite = {
             "forceJavac": True, # GRAALPYTHON_PROCESSOR is not compatible with ECJ
             "workingSets": "Truffle,Python",
             "spotbugsIgnoresGenerated": True,
+            # GR-60063: this disables all javac warnings
+            "javac.lint.overrides" : "none",
         },
 
         # GRAALPYTHON_UNIT_TESTS
