e15cbb5f470e14a70f71dc1e70b0d1723825e093
Tomas Stupka <tomas.stupka@oracle.com>
2024-12-19 17:40:02 +0100

    added enable-native-access=ALL-UNNAMED parameter to generated venv graalpy launcher

--- graalpython/org.graalvm.python.embedding.tools/src/org/graalvm/python/embedding/tools/vfs/VFSUtils.java
+++ graalpython/org.graalvm.python.embedding.tools/src/org/graalvm/python/embedding/tools/vfs/VFSUtils.java
@@ -284,7 +284,7 @@ public final class VFSUtils {
             if (!IS_WINDOWS) {
                 var script = String.format("""
                                 #!/usr/bin/env bash
-                                %s -classpath %s %s --python.Executable="$0" "$@"
+                                %s --enable-native-access=ALL-UNNAMED -classpath %s %s --python.Executable="$0" "$@"
                                 """,
                                 java,
                                 String.join(File.pathSeparator, classpath),
@@ -306,7 +306,7 @@ public final class VFSUtils {
                                 tl = os.path.join(r'%s')
                                 os.makedirs(Path(tl).parent.absolute(), exist_ok=True)
                                 shutil.copy(vl, tl)
-                                cmd = r'%s -classpath "%s" %s'
+                                cmd = r'%s --enable-native-access=ALL-UNNAMED -classpath "%s" %s --jvm --enable-native-access=ALL-UNNAMED'
                                 pyvenvcfg = os.path.join(os.path.dirname(tl), "pyvenv.cfg")
                                 with open(pyvenvcfg, 'w', encoding='utf-8') as f:
                                     f.write('venvlauncher_command = ')
