4af8cfdfd9e8267fb47d311e4663ee1768cd2554
Tim Felgentreff <tim.felgentreff@oracle.com>
2025-02-25 15:52:10 +0100

    Fix style checks

--- graalpython/com.oracle.graal.python.test.integration/src/com/oracle/graal/python/test/integration/interop/JavaInteropTest.java
+++ graalpython/com.oracle.graal.python.test.integration/src/com/oracle/graal/python/test/integration/interop/JavaInteropTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -869,7 +869,7 @@ public class JavaInteropTest {
         }
 
         @Test
-        public void javaExecutablesAsPythonFunctions() throws IOException {
+        public void javaExecutablesAsPythonFunctions() {
             Value inspectSignature = context.eval("python", "import inspect; inspect.signature");
             assertEquals("<Signature (string)>", inspectSignature.execute(new MyFunction()).toString());
 
