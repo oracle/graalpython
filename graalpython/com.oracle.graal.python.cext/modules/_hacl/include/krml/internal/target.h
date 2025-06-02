500d26d6f4c228d00ca529d02ceef908a537ff9c
Mohaned Qunaibit <mohaned.qunaibit@oracle.com>
2025-06-02 13:27:13 +0300

    update ASL license

--- graalpython/com.oracle.graal.python.cext/modules/_hacl/include/krml/internal/target.h
+++ graalpython/com.oracle.graal.python.cext/modules/_hacl/include/krml/internal/target.h
@@ -1,4 +1,5 @@
-/* Copyright (c) INRIA and Microsoft Corporation. All rights reserved.
+/* Copyright (c) 2025, Oracle and/or its affiliates.
+   Copyright (c) INRIA and Microsoft Corporation. All rights reserved.
    Licensed under the Apache 2.0 License. */
 
 #ifndef __KRML_TARGET_H
