ed4349b9d92e9761d8902a03a83b2e6c6ebace30
Michael Simacek <michael.simacek@oracle.com>
2025-06-26 10:21:06 +0200

    Remove unnecessary boundaries from PythonNativeWrapper.incRef/decRef

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/capi/PythonNativeWrapper.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/cext/capi/PythonNativeWrapper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, 2024, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -198,7 +198,6 @@ public abstract class PythonNativeWrapper implements TruffleObject {
             return MANAGED_REFCNT;
         }
 
-        @TruffleBoundary(allowInlining = true)
         public long incRef() {
             assert isNative();
             long pointer = HandlePointerConverter.pointerToStub(getNativePointer());
@@ -211,7 +210,6 @@ public abstract class PythonNativeWrapper implements TruffleObject {
             return IMMORTAL_REFCNT;
         }
 
-        @TruffleBoundary(allowInlining = true)
         public long decRef() {
             assert isNative();
             long pointer = HandlePointerConverter.pointerToStub(getNativePointer());
