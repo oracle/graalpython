f0db7ae11dda93e0fcd9a42d9b22f5b84fdfa463
Andrii Konchyn <andrii.konchyn@oracle.com>
2025-07-28 16:34:33 +0300

    Fix unicodedata methods delegation to _cpython_unicodedata and delegate only specified ones

--- graalpython/lib-graalpython/__graalpython__.py
+++ graalpython/lib-graalpython/__graalpython__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2020, 2025, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -73,7 +73,6 @@ def lazy_attributes_from_delegate(module, delegate_name, attributes, owner_modul
     :param on_import_error: a dict of replacement attributes in case of import error
     :return:
     """
-    attributes.append('__all__')
 
     def __getattr__(attr):
         if attr in attributes:
@@ -86,6 +85,7 @@ def lazy_attributes_from_delegate(module, delegate_name, attributes, owner_modul
                     raise
 
             new_globals = dict(**delegate_module.__dict__)
+            new_globals = { key: new_globals[key] for key in attributes }
             new_globals.update(**owner_module.__dict__)
             owner_module.__dict__.update(**new_globals)
 
