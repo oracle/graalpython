55c1690cb8b42ccdd60a5133b182fe7d38f0fd03
Tim Felgentreff <tim.felgentreff@oracle.com>
2025-10-02 10:51:00 +0200

    Add a micro benchmark for startup

---
+++ graalpython/com.oracle.graal.python.benchmarks/python/micro/startup.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+#
+# The Universal Permissive License (UPL), Version 1.0
+#
+# Subject to the condition set forth below, permission is hereby granted to any
+# person obtaining a copy of this software, associated documentation and/or
+# data (collectively the "Software"), free of charge and under any and all
+# copyright rights in the Software, and any and all patent rights owned or
+# freely licensable by each licensor hereunder covering either (i) the
+# unmodified Software as contributed to or provided by such licensor, or (ii)
+# the Larger Works (as defined below), to deal in both
+#
+# (a) the Software, and
+#
+# (b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
+# one is included with the Software each a "Larger Work" to which the Software
+# is contributed by such licensors),
+#
+# without restriction, including without limitation the rights to copy, create
+# derivative works of, display, perform, and distribute the Software and make,
+# use, sell, offer for sale, import, export, have made, and have sold the
+# Software and the Larger Work(s), and to sublicense the foregoing rights on
+# either these or other terms.
+#
+# This license is subject to the following condition:
+#
+# The above copyright notice and either this complete permission notice or at a
+# minimum a reference to the UPL must be included in all copies or substantial
+# portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+import subprocess, sys
+
+
+def __benchmark__(num=1000000):
+    while num > 0:
+        num -= 1
+        orig_vm_argv = sys.orig_argv
+        for i, arg in enumerate(orig_vm_argv):
+            if arg.endswith('.py'):
+                orig_vm_argv = orig_vm_argv[:i]
+                break
+        subprocess.check_call([
+            *orig_vm_argv,
+            "-I", # isolate from environment
+            "-S", # do not import site
+            "-Wignore", # ignore all warnings
+            "-B", # do not attempt to write pyc files
+            "-u", # do not add buffering wrappers around output streams
+            "-c",
+            "1"
+        ])
