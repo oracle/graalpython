908afa827cc7a7da5cfa1b5d701edc53a7f9a5b9
Virgil Calvez <virgil.calvez@oracle.com>
2025-12-09 11:08:19 +0100

    Merge oracle/graalpython master and fixes issues

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/compiler/bytecode_dsl/RootNodeCompiler.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/compiler/bytecode_dsl/RootNodeCompiler.java
@@ -648,7 +648,7 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
     /**
      * Emits a "line" tracing if either no tracing was emitted before, or if line number was
      * updated.
-     *
+     * 
      * @param b Builder for line tracing.
      */
     void endTraceLineChecked(SSTNode node, Builder b) {
@@ -1881,7 +1881,6 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
         @Override
         public Void visit(ExprTy.BoolOp node) {
             boolean newStatement = beginSourceSection(node, b);
-            b.beginBlock();
             emitTraceLineChecked(node, b);
 
             if (node.op == BoolOpTy.And) {
@@ -2055,7 +2054,6 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
         @Override
         public Void visit(ExprTy.Call node) {
             boolean newStatement = beginSourceSection(node, b);
-            b.beginBlock();
             emitTraceLineChecked(node, b);
             checkCaller(ctx.errorCallback, node.func);
             emitCall(node.func, node.args, node.keywords);
@@ -2848,9 +2846,7 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
              *     returnValue = e.value
              *     goto end
              *
-             * end:
-             * # Step 4: return returnValue
-             * load returnValue (result)
+             * end: # Step 4: resultValue local is assigned
              * @formatter:on
              */
             BytecodeLocal generator = b.createLocal();
@@ -4390,7 +4386,6 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
              */
             MatchCaseTy c = cases[index];
             boolean newStatement = beginSourceSection(c, b);
-            b.beginBlock();
             emitTraceLineChecked(cases[index], b);
 
             if (index != cases.length - 1) {
@@ -4495,7 +4490,6 @@ public final class RootNodeCompiler implements BaseBytecodeDSLVisitor<BytecodeDS
          */
         private void visitPattern(PatternTy pattern, PatternContext pc) {
             boolean newStatement = beginSourceSection(pattern, b);
-            b.beginBlock();
             emitTraceLineChecked(pattern, b);
             if (pattern instanceof PatternTy.MatchAs matchAs) {
                 doVisitPattern(matchAs, pc);
