cd58d9a5f8053343dccfd64dcf24a570dc6a0bdc
Michael Simacek <michael.simacek@oracle.com>
2026-01-08 15:30:10 +0100

    GraalPyArray_Data should return NULL for empty arrays

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/cext/PythonCextArrayBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/modules/cext/PythonCextArrayBuiltins.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -93,7 +93,11 @@ public final class PythonCextArrayBuiltins {
         static Object get(PArray array,
                         @Bind Node inliningTarget,
                         @Cached ArrayNodes.EnsureNativeStorageNode ensureNativeStorageNode) {
-            return ensureNativeStorageNode.execute(inliningTarget, array).getPtr();
+            if (array.getBytesLength() > 0) {
+                return ensureNativeStorageNode.execute(inliningTarget, array).getPtr();
+            } else {
+                return getNativeNull(inliningTarget);
+            }
         }
     }
 
@@ -104,7 +108,6 @@ public final class PythonCextArrayBuiltins {
                         @Bind Node inliningTarget,
                         @Cached ArrayNodes.EnsureNativeStorageNode ensureNativeStorageNode,
                         @Cached TruffleString.SwitchEncodingNode switchEncodingNode,
-                        @Cached TruffleString.CopyToByteArrayNode copyToByteArrayNode,
                         @Cached CApiTransitions.PythonToNativeNewRefNode toNativeNewRefNode,
                         @Cached CStructAccess.WritePointerNode writePointerNode,
                         @Cached CStructAccess.WriteLongNode writeLongNode,
