d6afcec17c19d0cc593372e68f0d72170e781831
Michael Simacek <michael.simacek@oracle.com>
2026-04-29 19:25:07 +0200

    Redirect Graal logs in stdout-sensitive tests

--- graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_shutdown.py
+++ graalpython/com.oracle.graal.python.test/src/tests/cpyext/test_shutdown.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, 2025, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2023, 2026, Oracle and/or its affiliates. All rights reserved.
 # DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
 #
 # The Universal Permissive License (UPL), Version 1.0
@@ -50,8 +50,9 @@ ENV = dict(os.environ)
 ENV['PYTHONPATH'] = str(DIR.parent.parent)
 ARGS = []
 if sys.implementation.name == 'graalpy':
-    ARGS = ['--experimental-options', '--python.EnableDebuggingBuiltins']
+    ARGS = ['--experimental-options', f'--log.file={os.devnull}', '--python.EnableDebuggingBuiltins']
     if not __graalpython__.is_native:
+        ARGS += [f'--vm.Djdk.graal.LogFile={os.devnull}']
         ARGS += [f'--vm.Dpython.EnableBytecodeDSLInterpreter={str(__graalpython__.is_bytecode_dsl_interpreter).lower()}']
 COMMAND = [sys.executable, *ARGS, str(MODULE_PATH)]
 
