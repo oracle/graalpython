ceb4b64ba7f6e18a44e48ffbaeb329b446099dbf
Tim Felgentreff <tim.felgentreff@oracle.com>
2026-06-04 10:05:17 +0200

    Move setting func.__kwdefaults__ behind boundary

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/PFunction.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/function/PFunction.java
@@ -240,6 +240,7 @@ public final class PFunction extends PythonObject {
         return kwDefaultDict;
     }
 
+    @TruffleBoundary
     public void setKwDefaultsDict(PDict defaults) {
         this.codeStableAssumption.invalidate("kw defaults changed for function " + getName());
         this.finalKwDefaultValues = null; // avoid leak, and make code that wrongly uses it crash
