e74b3d9552e3994e14a25a2b7e904264f8f83959
Tim Felgentreff <tim.felgentreff@oracle.com>
2026-06-26 20:37:28 +0200

    Avoid Java-level Windows last-error reset for mmap

--- graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/mmap/MMapBuiltins.java
+++ graalpython/com.oracle.graal.python/src/com/oracle/graal/python/builtins/objects/mmap/MMapBuiltins.java
@@ -135,7 +135,6 @@ import com.oracle.graal.python.runtime.PosixSupport;
 import com.oracle.graal.python.runtime.PosixSupportLibrary;
 import com.oracle.graal.python.runtime.PosixSupportLibrary.PosixException;
 import com.oracle.graal.python.runtime.PythonContext;
-import com.oracle.graal.python.runtime.nativeaccess.NativeContext;
 import com.oracle.graal.python.runtime.object.PFactory;
 import com.oracle.graal.python.runtime.sequence.storage.ByteSequenceStorage;
 import com.oracle.graal.python.util.OverflowException;
@@ -309,11 +308,7 @@ public final class MMapBuiltins extends PythonBuiltins {
                 throw constructAndRaiseNode.get(inliningTarget).raiseOSErrorFromPosixException(frame, e);
             }
             PythonContext context = PythonContext.get(inliningTarget);
-            PMMap mmap = PFactory.createMMap(context, clazz, getInstanceShape.execute(clazz), mmapHandle, dupFd, length, access);
-            if (PythonLanguage.getPythonOS() == PythonOS.PLATFORM_WIN32) {
-                NativeContext.setLastError(0);
-            }
-            return mmap;
+            return PFactory.createMMap(context, clazz, getInstanceShape.execute(clazz), mmapHandle, dupFd, length, access);
         }
 
         @Specialization(guards = "isIllegal(fd)")
