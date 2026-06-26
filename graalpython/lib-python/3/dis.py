629fbc68ca4d507b433bfbbcb6f532a330e98ce5
Michael Simacek <michael.simacek@oracle.com>
2026-06-26 16:33:51 +0200

    Make dis importable

--- graalpython/lib-python/3/dis.py
+++ graalpython/lib-python/3/dis.py
@@ -5,62 +5,15 @@ import types
 import collections
 import io
 
-from opcode import *
-from opcode import (
-    __all__ as _opcodes_all,
-    _cache_format,
-    _inline_cache_entries,
-    _nb_ops,
-    _intrinsic_1_descs,
-    _intrinsic_2_descs,
-    _specializations,
-    _specialized_opmap,
-)
-
-from _opcode import get_executor
+# GraalPy change: remove all the _opcode imports and dependent stuff
 
 __all__ = ["code_info", "dis", "disassemble", "distb", "disco",
            "findlinestarts", "findlabels", "show_code",
-           "get_instructions", "Instruction", "Bytecode"] + _opcodes_all
-del _opcodes_all
+           "get_instructions", "Instruction", "Bytecode"]
 
 _have_code = (types.MethodType, types.FunctionType, types.CodeType,
               classmethod, staticmethod, type)
 
-CONVERT_VALUE = opmap['CONVERT_VALUE']
-
-SET_FUNCTION_ATTRIBUTE = opmap['SET_FUNCTION_ATTRIBUTE']
-FUNCTION_ATTR_FLAGS = ('defaults', 'kwdefaults', 'annotations', 'closure')
-
-ENTER_EXECUTOR = opmap['ENTER_EXECUTOR']
-LOAD_CONST = opmap['LOAD_CONST']
-RETURN_CONST = opmap['RETURN_CONST']
-LOAD_GLOBAL = opmap['LOAD_GLOBAL']
-BINARY_OP = opmap['BINARY_OP']
-JUMP_BACKWARD = opmap['JUMP_BACKWARD']
-FOR_ITER = opmap['FOR_ITER']
-SEND = opmap['SEND']
-LOAD_ATTR = opmap['LOAD_ATTR']
-LOAD_SUPER_ATTR = opmap['LOAD_SUPER_ATTR']
-CALL_INTRINSIC_1 = opmap['CALL_INTRINSIC_1']
-CALL_INTRINSIC_2 = opmap['CALL_INTRINSIC_2']
-LOAD_FAST_LOAD_FAST = opmap['LOAD_FAST_LOAD_FAST']
-STORE_FAST_LOAD_FAST = opmap['STORE_FAST_LOAD_FAST']
-STORE_FAST_STORE_FAST = opmap['STORE_FAST_STORE_FAST']
-
-CACHE = opmap["CACHE"]
-
-_all_opname = list(opname)
-_all_opmap = dict(opmap)
-for name, op in _specialized_opmap.items():
-    # fill opname and opmap
-    assert op < len(_all_opname)
-    _all_opname[op] = name
-    _all_opmap[name] = op
-
-deoptmap = {
-    specialized: base for base, family in _specializations.items() for specialized in family
-}
 
 def _try_compile(source, name):
     """Attempts to compile the given source, first as an expression and
