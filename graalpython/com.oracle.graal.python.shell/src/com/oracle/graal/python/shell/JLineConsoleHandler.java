13300d900a78f6b5abed4dd4388ebdeae6f3f035
Michael Simacek <michael.simacek@oracle.com>
2026-07-16 16:53:30 +0200

    Don't peristently enable JLine signal handlers

--- graalpython/com.oracle.graal.python.shell/src/com/oracle/graal/python/shell/JLineConsoleHandler.java
+++ graalpython/com.oracle.graal.python.shell/src/com/oracle/graal/python/shell/JLineConsoleHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2025, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2017, 2026, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * The Universal Permissive License (UPL), Version 1.0
@@ -94,7 +94,7 @@ public class JLineConsoleHandler extends ConsoleHandler {
     public void initializeReadline(Value readlineModule) {
         Terminal terminal;
         try {
-            terminal = TerminalBuilder.builder().jna(false).streams(inputStream, outputStream).system(true).signalHandler(Terminal.SignalHandler.SIG_IGN).build();
+            terminal = TerminalBuilder.builder().jna(false).streams(inputStream, outputStream).system(true).nativeSignals(false).signalHandler(Terminal.SignalHandler.SIG_IGN).build();
         } catch (IOException ex) {
             throw new RuntimeException("unexpected error opening console reader", ex);
         }
