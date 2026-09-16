63e40dba67c3678087c122dee1b0fa8201336372
Michael Simacek <michael.simacek@oracle.com>
2026-09-16 10:04:27 +0200

    Add a leak tests for importing a module without bytecode

--- mx.graalpython/mx_graalpython.py
+++ mx.graalpython/mx_graalpython.py
@@ -1029,6 +1029,9 @@ def leak_tests(args):
     # test leaks with shared engine Python code only
     run_leak_launcher(["--shared-engine", "--code", "pass"])
     run_leak_launcher(["--shared-engine", "--repeat-and-check-size", "250", "--null-stdout", "--code", "print('hello')"])
+    run_leak_launcher(["--shared-engine", "--repeat-and-check-size", "100", "--null-stdout", "--code", "import ipaddress"])
+    run_leak_launcher(["--shared-engine", "--repeat-and-check-size", "100", "--null-stdout", "--code", "import ipaddress",
+                       "--python.PyCachePrefix=/dev/null"])
     # test leaks with shared engine when some C module code is involved
     if HAS_JEP_454:
         run_leak_launcher([
